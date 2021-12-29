97b6a06cd22c71330760a10703456c77e00e1ef9
Phillip 1 <phillipdavis2468@gmail.com>
2021-12-29 20:43:01 +1100

    refactored the trending table

---
+++ frontend/src/components/TrendingTable.jsx
@@ -0,0 +1,81 @@
+import MUIDataTable from "mui-datatables";
+import {useEffect, useState} from "react";
+import {Typography} from "@mui/material";
+import styled from "styled-components";
+
+const PercentageText = styled.span`
+  display: inline-flex;
+  padding: 5px;
+  font-size: 1em;
+  border-radius: 8px;
+  background-color: ${(props) => props.changePercentage > 0 ? "darkseagreen" : "firebrick"};
+  color: ${(props) => props.changePercentage > 0 ? "darkgreen" : "darkred"};
+`
+
+const TriangleSymbol = styled.div`
+  display: inline-block;
+  border-right: 8px solid transparent;
+  align-self: center;
+`
+
+const TrendingTable = () => {
+    const [trendingStocks, setTrendingStocks] = useState([]);
+
+    const columnDefinitions = [
+        {name: 'symbol', label: 'Ticker symbol'},
+        {name: 'shortName', label: 'Company name'},
+        {name: 'regularMarketPrice', label: 'Price'},
+        {name: 'regularMarketDayHigh', label: 'High price'},
+        {name: 'regularMarketDayLow', label: 'Low price'},
+        {
+            name: 'regularMarketChange', label: 'Change %', options: {
+                customBodyRender: (value, tableMeta, updateValue) => {
+                    return (
+                        <>
+                            <PercentageText changePercentage={value}>
+                                {value > 0 ? <TriangleSymbol>&#x25B2;</TriangleSymbol> : <TriangleSymbol>&#x25BC;</TriangleSymbol>}
+                                <Typography align={"center"}>{value.toFixed(2)}</Typography>
+                            </PercentageText>
+                        </>
+                    )
+                }
+            }
+        },
+        {name: 'marketCap', label: 'Market cap'},
+    ]
+
+    useEffect(() => {
+        fetch('/trending-tickers')
+            .then((res) => res.json())
+            .then((res) => {
+                setTrendingStocks(Object.values(res))
+            })
+    }, [])
+
+    const options = {
+        download: false,
+        print: false,
+        search: false,
+        sortFilterList: false,
+        viewColumns: false,
+        selectableRows: 'none',
+        rowHover: false,
+        pagination: false,
+        fixedSelectColumn: false,
+        fixedHeader: false,
+        filterArrayFullMatch: false,
+    }
+
+    return (
+        <>
+            <MUIDataTable
+                title={"Trending stocks today (from Yahoo finance)"}
+                data={trendingStocks}
+                columns={columnDefinitions}
+                options={options}
+            />
+        </>
+    )
+}
+
+export default TrendingTable;
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,59 +1,8 @@
-import {useEffect, useState} from "react";
-import {Container, Grid, Typography} from "@mui/material";
+import {Container, Grid} from "@mui/material";
 import Header from "../components/Header";
-import MUIDataTable from "mui-datatables";
-import styled from "styled-components"
-
-const PercentageText = styled.span`
-  display: inline-flex;
-  padding: 5px;
-  font-size: 1em;
-  border-radius: 8px;
-  background-color: ${(props) => props.changePercentage > 0 ? "darkseagreen" : "firebrick"};
-  color: ${(props) => props.changePercentage > 0 ? "darkgreen" : "darkred"};
-`
-
-const TriangleSymbol = styled.div`
-  display: inline-block;
-  border-right: 8px solid transparent;
-  align-self: center;
-`
+import TrendingTable from "../components/TrendingTable";
 
 const Homepage = () => {
-
-    const [trendingStocks, setTrendingStocks] = useState([]);
-
-    const columnDefinitions = [
-        {name: 'symbol', label: 'Ticker symbol'},
-        {name: 'shortName', label: 'Company name'},
-        {name: 'regularMarketPrice', label: 'Price'},
-        {name: 'regularMarketDayHigh', label: 'High price'},
-        {name: 'regularMarketDayLow', label: 'Low price'},
-        {
-            name: 'regularMarketChange', label: 'Change %', options: {
-                customBodyRender: (value, tableMeta, updateValue) => {
-                    return (
-                        <>
-                            <PercentageText changePercentage={value}>
-                                {value > 0 ? <TriangleSymbol>&#x25B2;</TriangleSymbol> : <TriangleSymbol>&#x25BC;</TriangleSymbol>}
-                                <Typography align={"center"}>{value.toFixed(2)}</Typography>
-                            </PercentageText>
-                        </>
-                    )
-                }
-            }
-        },
-        {name: 'marketCap', label: 'Market cap'},
-    ]
-
-    useEffect(() => {
-        fetch('/trending-tickers')
-            .then((res) => res.json())
-            .then((res) => {
-                setTrendingStocks(Object.values(res))
-            })
-    }, [])
-
 
     return (
         <Grid container spacing={2} direction={"column"}>
@@ -63,24 +12,7 @@ const Homepage = () => {
 
             <Grid item>
                 <Container>
-                    <MUIDataTable
-                        title={"Trending stocks today in the US"}
-                        data={trendingStocks}
-                        columns={columnDefinitions}
-                        options={{
-                            download: false,
-                            print: false,
-                            search: false,
-                            sortFilterList: false,
-                            viewColumns: false,
-                            selectableRows: 'none',
-                            rowHover: false,
-                            pagination: false,
-                            fixedSelectColumn: false,
-                            fixedHeader: false,
-                            filterArrayFullMatch: false,
-                        }
-                        }/>
+                   <TrendingTable/>
                 </Container>
             </Grid>
 
