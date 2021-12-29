0d7a19c0c5c6701626b1b7539807e4a037039130
Phillip 1 <phillipdavis2468@gmail.com>
2021-12-29 17:01:54 +1100

    added a litle spacing between elements

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,5 +1,5 @@
 import {useEffect, useState} from "react";
-import {Container} from "@mui/material";
+import {Container, Grid, Typography} from "@mui/material";
 import Header from "../components/Header";
 import MUIDataTable from "mui-datatables";
 
@@ -13,7 +13,14 @@ const Homepage = () => {
         {name: 'regularMarketPrice', label: 'Price'},
         {name: 'regularMarketDayHigh', label: 'High price'},
         {name: 'regularMarketDayLow', label: 'Low price'},
-        {name: 'regularMarketChange', label: 'Change %'},
+        {name: 'regularMarketChange', label: 'Change %', options: {
+            customBodyRender: (value, tableMeta, updateValue) => {
+                return (
+                    <Typography>{value.toFixed(2)}</Typography>
+                )
+            }
+            }
+        },
         {name: 'marketCap', label: 'Market cap'},
     ]
 
@@ -27,32 +34,35 @@ const Homepage = () => {
 
 
     return (
-        <>
-            <Header/>
-            <Container>
-                <MUIDataTable
-                    title={"Trending stocks today"}
-                    data={trendingStocks}
-                    columns={columnDefinitions}
-                    options={{
-                        download: false,
-                        print: false,
-                        search: false,
-                        selectableRowsHeader: false,
-                        sortFilterList: false,
-                        viewColumns: false,
-                        sort: false,
-                        selectableRows: 'none',
-                        rowHover: false,
-                        pagination: false,
-                        fixedSelectColumn: false,
-                        fixedHeader: false,
-                        filterArrayFullMatch: false,
-                        filter: false
+        <Grid container spacing={2} direction={"column"}>
+            <Grid item>
+                <Header/>
+            </Grid>
+
+            <Grid item>
+                <Container>
+                    <MUIDataTable
+                        title={"Trending stocks today in the US"}
+                        data={trendingStocks}
+                        columns={columnDefinitions}
+                        options={{
+                            download: false,
+                            print: false,
+                            search: false,
+                            sortFilterList: false,
+                            viewColumns: false,
+                            selectableRows: 'none',
+                            rowHover: false,
+                            pagination: false,
+                            fixedSelectColumn: false,
+                            fixedHeader: false,
+                            filterArrayFullMatch: false,
                         }
-                    }/>
-            </Container>
-        </>
+                        }/>
+                </Container>
+            </Grid>
+
+        </Grid>
 
     )
 }
