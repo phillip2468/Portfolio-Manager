2868ed50a42de9c49852302b518f0c63a7ec6b71
Phillip 1 <phillipdavis2468@gmail.com>
2022-01-05 18:54:12 +1100

    now can show order by industry best performining sector

--- backend/money_maker/quote/routes.py
+++ backend/money_maker/quote/routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify
 from money_maker.extensions import db
 from money_maker.helpers import object_as_dict
 from money_maker.models.ticker_prices import TickerPrice
-from sqlalchemy import select
+from sqlalchemy import select, func, alias, text
 
 quote_bp = Blueprint('quote_bp', __name__)
 
@@ -11,3 +11,11 @@ quote_bp = Blueprint('quote_bp', __name__)
 def ticker_information(ticker):
     stmt: select = select(TickerPrice).filter(TickerPrice.symbol == ticker)
     return jsonify([object_as_dict(element) for element in db.session.execute(stmt).one()])
+
+
+@quote_bp.route("/quote/industry/market-change/<order>")
+def market_change_by_industry(order):
+    stmt: select = select(TickerPrice.industry, func.avg(TickerPrice.market_change_percentage).label("Average"),
+                          func.count(TickerPrice.industry)).where(TickerPrice.industry.is_not(None))\
+                        .group_by(TickerPrice.industry).order_by(text(f""""Average" {order}"""))
+    return jsonify([element._asdict() for element in db.session.execute(stmt).all()])
