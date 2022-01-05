9b6ba0c382e359a2a420e0460d3d19163b407001
Phillip Davis <phillipdavis2468@gmail.com>
2022-01-05 15:23:42 +1100

    ascending and descending works

--- backend/money_maker/trending/routes.py
+++ backend/money_maker/trending/routes.py
@@ -10,13 +10,13 @@ trending_bp = Blueprint('trending_bp', __name__)
 
 @trending_bp.route("/trending/<exchange>-<market_cap>-<order>")
 def ticker_information(exchange, market_cap, order):
-    stmt: select = select(tP.symbol, tP.market_change_percentage, tP.market_cap, cast(func.timezone('AESST', cast(tP.last_updated, sqlalchemy.TIME)), sqlalchemy.Text))\
+    stmt: select = select(tP.symbol, tP.market_change_percentage, tP.market_cap,
+                          cast(func.timezone('AESST', cast(tP.last_updated, sqlalchemy.TIME)), sqlalchemy.Text))\
         .where(tP.market_change_percentage.isnot(None))\
         .where(tP.market_cap > market_cap)\
         .where(tP.exchange == exchange)\
-        .order_by(tP.market_change_percentage.asc())\
+        .order_by(text(f"{tP.market_change_percentage.name} {order}"))\
         .limit(10)
-    print(stmt)
 
     # noinspection PyProtectedMember
     return jsonify([element._asdict() for element in db.session.execute(stmt).all()])
