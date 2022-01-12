e1d46efa56c5792ce4be069f32aef4d7270433ae
Phillip Davis <phillipdavis2468@gmail.com>
2022-01-12 16:12:50 +1100

    making a custom search box

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,9 +1,33 @@
 import {Container, Grid} from "@mui/material";
 import Header from "../components/Header";
 import TrendingTable from "../components/TrendingTable";
+import SearchIcon from '@mui/icons-material/Search';
+import styled from "styled-components";
+
 
 const Homepage = () => {
-
+    const data = [
+        {
+            key: "john",
+            value: "John Doe",
+        },
+        {
+            key: "jane",
+            value: "Jane Doe",
+        },
+        {
+            key: "mary",
+            value: "Mary Phillips",
+        },
+        {
+            key: "robert",
+            value: "Robert",
+        },
+        {
+            key: "karius",
+            value: "Karius",
+        },
+    ];
     return (
         <Grid container spacing={2} direction={"column"}>
             <Grid item>
@@ -11,7 +35,20 @@ const Homepage = () => {
             </Grid>
 
             <Grid item>
+                <Container maxWidth={'xs'}>
+                    <div style={{display: "flex"}}>
+                        <input
+                            placeholder={"Search for stocks"}
+                            style={{flex: 1, fontSize: "large"}}
+                        />
+                    </div>
+                </Container>
+
+            </Grid>
+
+            <Grid item>
                 <Container>
+
                    <TrendingTable/>
                 </Container>
             </Grid>
