5d3567760b84cdefaa8d7ac02d390bf8b700892f
Phillip 1 <phillipdavis2468@gmail.com>
2022-01-14 21:02:20 +1100

    added a quick section for popular stocks in aus

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -3,9 +3,19 @@ import Header from "../components/Header";
 import TrendingTable from "../components/TrendingTable";
 import SearchBar from "../components/SearchBar";
 import {useEffect, useState} from "react";
+import DataTable from "react-data-table-component"
+import styled from "styled-components"
+
+
+const TrendingStocks = styled.div`
+  display: flex;
+  flex-direction: column;
+`
 
 const Homepage = () => {
     const [allStocks, setAllStocks] = useState([])
+
+    const [popularStocks, setPopularStocks] = useState([])
 
     useEffect(()=> {
             fetch('/quote/search')
@@ -15,6 +25,42 @@ const Homepage = () => {
                 })
         }
     , [])
+
+    useEffect(()=> {
+        fetch('/actively-traded')
+            .then((res) => res.json())
+            .then((res)=> {
+                setPopularStocks(res)
+            })
+    }, [])
+
+    const columns = [
+        {
+            'name': 'symbol',
+            'selector': row => row.symbol,
+            'width': '100px'
+        },
+        {
+            'name': 'Name',
+            'selector': row => row.stock_name,
+            'compact': true,
+            'width': '250px'
+        }
+        ,
+        {
+            'name': 'price',
+            'selector': row => row.market_current_price,
+            'compact': true,
+            'width': '100px'
+        },
+        {
+            'name': 'change %',
+            'selector': row => row.market_change_percentage,
+            'compact': true,
+            'width': "100px",
+            'format': row => (row.market_change_percentage * 100).toFixed(2)
+        }
+    ]
 
     return (
         <Grid container spacing={2} direction={"column"}>
@@ -27,6 +73,20 @@ const Homepage = () => {
                     placeholder={"Search for stocks"}
                     data={allStocks}
                 />
+            </Grid>
+
+            <Grid item>
+                <Container sx={{width: "600px"}}>
+                    <DataTable
+                        title={"Popular stocks today"}
+                        striped={true}
+                        dense={true}
+                        highlightOnHover={true}
+                        theme={"dark"}
+                        columns={columns}
+                        data={popularStocks}
+                    />
+                </Container>
             </Grid>
 
             <Grid item>
