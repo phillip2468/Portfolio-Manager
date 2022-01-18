1a955b3fae143ea5fcac7b7bf1bb8a44081df84d
Phillip Davis <phillipdavis2468@gmail.com>
2022-01-18 18:26:19 +1100

    got celery beat to work

--- backend/money_maker/celery_app.py
+++ backend/money_maker/celery_app.py
@@ -1,4 +1,10 @@
 from money_maker.app import init_celery
+from money_maker.tasks.task import update_asx_prices, add_together
+from money_maker.extensions import celery
 
 app = init_celery()
 
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(60, update_asx_prices, name="update_asx_prices")
