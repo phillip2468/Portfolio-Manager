6c1aa101f6d63051e3ea671fbaf912f58df0f93e
Phillip Davis <phillipdavis2468@gmail.com>
2022-01-19 16:39:27 +1100

    refactored some stuff

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -1,17 +1,16 @@
 import React, {useEffect, useState} from 'react';
-import SearchIcon from "@material-ui/icons/Search";
 import {
     DataResult,
     OuterContainer,
-    SearchBox,
-    SearchIconBox,
-    SearchInput,
-    SearchResultsGrid, StocksNameTicker, StocksPercentage, StocksPrice,
+    SearchResultsGrid,
+    StocksNameTicker,
+    StocksPercentage,
+    StocksPrice,
     TriangleSymbol
 } from "./styled";
 import {useNavigate} from "react-router-dom";
+import SearchInputBox from "./components/SearchInputBox";
 //https://stackoverflow.com/questions/42987939/styled-components-organization
-
 
 
 const SearchBar = ({placeholder}) => {
@@ -20,15 +19,16 @@ const SearchBar = ({placeholder}) => {
     const [typedInput, setTypedInput] = useState("");
     const [filteredData, setFilteredData] = useState([]);
     const [allStocksInfo, setAllStocksInfo] = useState([])
+    const [showResults, setShowResults] = useState(false);
 
-    useEffect(()=> {
+    useEffect(() => {
             fetch('/search')
                 .then((res) => res.json())
                 .then((res) => {
                     setAllStocksInfo(Object.values(res))
                 })
         }
-    , [])
+        , [])
 
 
     const handleSearch = (event) => {
@@ -45,27 +45,14 @@ const SearchBar = ({placeholder}) => {
         }
     }
 
-    const [showResults, setShowResults] = useState(false);
-
     return (
         <OuterContainer>
-            <SearchBox>
-                <SearchInput
-                    type={"text"}
-                    placeholder={placeholder}
-                    value={typedInput}
-                    onChange={handleSearch}
-                    onBlur={() => setShowResults(false)}
-                    onFocus={()=> setShowResults(true)}
-                />
-                <SearchIconBox>
-                    <SearchIcon/>
-                </SearchIconBox>
-            </SearchBox>
+            <SearchInputBox placeholder={placeholder} value={typedInput} onChange={handleSearch}
+                            onBlur={() => setShowResults(false)} onFocus={() => setShowResults(true)}/>
             <div style={{position: "relative", display: showResults === false ? "none" : "block"}}>
                 {typedInput.length !== 0 && (
                     <DataResult>
-                        {filteredData.map((item, key)=> {
+                        {filteredData.map((item, key) => {
                             return <SearchResultsGrid
                                 key={key}
                                 onClick={() => navigate(`/${item.symbol}`)}
@@ -80,7 +67,8 @@ const SearchBar = ({placeholder}) => {
                                 </StocksPrice>
 
                                 <StocksPercentage percentageChange={item.change}>
-                                    {item.change > 0 ? <TriangleSymbol>&#x25B2;</TriangleSymbol> : <TriangleSymbol>&#x25BC;</TriangleSymbol>}
+                                    {item.change > 0 ? <TriangleSymbol>&#x25B2;</TriangleSymbol> :
+                                        <TriangleSymbol>&#x25BC;</TriangleSymbol>}
                                     {(parseFloat(item.change) * 100).toFixed(2)}
                                 </StocksPercentage>
                             </SearchResultsGrid>
---
+++ frontend/src/components/SearchBar/components/SearchInputBox.jsx
@@ -0,0 +1,30 @@
+import {SearchBox, SearchIconBox, SearchInput} from "../styled";
+import SearchIcon from "@material-ui/icons/Search";
+import * as PropTypes from "prop-types";
+import React from "react";
+
+const SearchInputBox = (props) => {
+    return <SearchBox>
+        <SearchInput
+            type={"text"}
+            placeholder={props.placeholder}
+            value={props.value}
+            onChange={props.onChange}
+            onBlur={props.onBlur}
+            onFocus={props.onFocus}
+        />
+        <SearchIconBox>
+            <SearchIcon/>
+        </SearchIconBox>
+    </SearchBox>;
+}
+
+SearchInputBox.propTypes = {
+    placeholder: PropTypes.any,
+    value: PropTypes.string,
+    onChange: PropTypes.func,
+    onBlur: PropTypes.func,
+    onFocus: PropTypes.func
+};
+
+export default SearchInputBox;
