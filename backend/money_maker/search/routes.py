d67babe7ab91a093101d789cc34cb545add495b2
Phillip 1 <phillipdavis2468@gmail.com>
2022-01-20 17:19:39 +1100

    built

--- backend/money_maker/search/routes.py
+++ backend/money_maker/search/routes.py
@@ -10,6 +10,6 @@ def search_for_companies(keyword):
     keyword = "%{}%".format(keyword).upper()
     results = db.session.query(tP.stock_id.label("key"), tP.stock_name, tP.symbol,
                                tP.market_current_price.label("price"), tP.market_change_percentage.label("change"))\
-        .filter((tP.stock_name.ilike(keyword)) | (tP.symbol.ilike(keyword))).order_by(tP.market_volume.desc().nullslast())\
-        .limit(4).all()
+        .filter((tP.stock_name.ilike(keyword)) | (tP.symbol.ilike(keyword)))\
+        .order_by(tP.market_volume.desc().nullslast()).limit(4).all()
     return jsonify([dict(e) for e in results])
