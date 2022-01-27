28bc13788c668ea492efc5d90b74a39e34200b0a
Phillip Davis <phillipdavis2468@gmail.com>
2022-01-27 14:02:59 +1100

    changed the table

--- backend/money_maker/auth/routes.py
+++ backend/money_maker/auth/routes.py
@@ -1,5 +1,7 @@
-
 from flask import Blueprint, jsonify, request
+from money_maker.extensions import jwt_manager, db, bcrypt
+from money_maker.models.user import User
+from flask_jwt_extended import create_access_token
 
 auth_bp = Blueprint("auth_bp", __name__, url_prefix="/auth")
 
@@ -17,3 +19,40 @@ def login():
     email = req.get("email", None)
     password = req.get("password", None)
 
+    user: User = db.session.query(User).filter(User.email == email).one_or_none()
+    if not email or not password or not user:
+        return jsonify("Missing credentials"), 400
+
+    if bcrypt.check_password_hash(bcrypt.generate_password_hash(password), user.hashed_password):
+        access_token = create_access_token(identity=user.user_id)
+        return jsonify(access_token), 200
+    else:
+        return jsonify("Invalid login details"), 400
+
+
+@jwt_manager.user_identity_loader
+def user_identity_lookup(user: User):
+    """
+    Register a callback function that takes whatever object is passed in as the
+    identity when creating JWTs and converts it to a JSON serializable format.
+
+    :param user:
+    :return:
+    """
+    return user.user_id
+
+
+@jwt_manager.user_lookup_loader
+def user_lookup_callback(_jwt_header, jwt_data):
+    """
+     Register a callback function that loads a user from your database whenever
+     a protected route is accessed. This should return any python object on a
+     successful lookup, or None if the lookup failed for any reason (for example
+     if the user has been deleted from the database).
+
+    :param _jwt_header:
+    :param jwt_data:
+    :return:
+    """
+    identity = jwt_data["sub"]
+    return db.session.query(User).filter(User.user_id == identity).one_or_none()
