ce80573b1920b17a8a06632be3de1e9b7866acb6
phillip2468 <54766922+phillip2468@users.noreply.github.com>
2022-01-28 19:24:46 +1100

    added some swagger

--- backend/money_maker/models/user.py
+++ backend/money_maker/models/user.py
@@ -1,12 +1,15 @@
-from sqlalchemy import Column, Integer, Text, Boolean, TIMESTAMP, func
-
 from money_maker.extensions import db
+from safrs import SAFRSBase
+from sqlalchemy import TIMESTAMP, Column, Integer, Text, func
 
 # This model inspired by below link
 # https://flask-praetorian.readthedocs.io/en/latest/notes.html#requirements-for-the-user-class
 
 
-class User(db.Model):
+class User(SAFRSBase, db.Model):
+    """
+    description: all the users in the database
+    """
     __tablename__ = 'user'
     user_id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(Text, unique=True)
