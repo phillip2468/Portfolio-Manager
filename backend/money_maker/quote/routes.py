3a495867ff2af9ec623e290839261a9f268954f3
phillip2468 <54766922+phillip2468@users.noreply.github.com>
2022-01-29 19:02:33 +1100

    made small change to make it more consistent

--- backend/money_maker/quote/routes.py
+++ backend/money_maker/quote/routes.py
@@ -1,7 +1,6 @@
 import yahooquery
 from flask import Blueprint, jsonify
-from money_maker.extensions import cache, db
-from money_maker.helpers import object_as_dict
+from money_maker.extensions import db
 from money_maker.models.ticker_prices import TickerPrice as tP
 from money_maker.models.ticker_prices import \
     ticker_price_schema as ticker_schema
@@ -12,6 +11,7 @@ quote_bp = Blueprint("quote_bp", __name__, url_prefix="/quote")
 
 @quote_bp.route("/<category>/market-change/<order>")
 def market_change_by_industry(category, order):
+    # TODO
     industry_sector = getattr(tP, category)
     stmt: select = select(industry_sector, func.avg(tP.market_change_percentage).label("Average"),
                           func.count(industry_sector)).where(industry_sector.is_not(None)) \
