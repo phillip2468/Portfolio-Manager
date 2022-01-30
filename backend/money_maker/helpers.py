b8c1aa0595e42f5ca975c82a33eee70cac47e706
phillip2468 <54766922+phillip2468@users.noreply.github.com>
2022-01-30 20:36:32 +1100

    Some errors corcenring the json data with marketindex.com

--- backend/money_maker/helpers.py
+++ backend/money_maker/helpers.py
@@ -2,7 +2,7 @@ import requests
 from requests import Response
 from sqlalchemy import inspect
 
-header: dict[str, str] = {
+header = {
     "Accept": "*/*",
     "Accept-Encoding": "gzip, deflate",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:95.0) Gecko/20100101 Firefox/95.0"
@@ -18,7 +18,9 @@ def sync_request(url: str) -> requests.Response:
     :return: The response as a json.
     :rtype: requests.Response
     """
-    response: requests.Response = requests.get(url, headers=header)
+    response = requests.get(url)
+    response.raise_for_status()
+    print(response.status_code)
     return response.json()
 
 
@@ -27,7 +29,7 @@ def object_as_dict(obj):
             for c in inspect(obj).mapper.column_attrs}
 
 
-def market_index_ticker() -> Response:
+def asx_tickers_json() -> Response:
     """
     Gets the code, status and title of all ASX listed stocks.
     All results are held in a list of dictionaries.
