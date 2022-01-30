d991186da7a98bda88ab9181af0b3c2fc435655b
phillip2468 <54766922+phillip2468@users.noreply.github.com>
2022-01-30 19:05:54 +1100

    added better comments for the quotes page

--- backend/money_maker/quote/routes.py
+++ backend/money_maker/quote/routes.py
@@ -49,17 +49,34 @@ def market_change_by_attribute(attribute: str, order: str) -> flask.Response:
     results = db.session.query(ticker_price_field, func.avg(tP.market_change_percentage).label("Average"),
                                func.count(ticker_price_field)).filter(ticker_price_field.isnot(None)) \
         .group_by(ticker_price_field).order_by(text(f""""Average" {order.upper()}""")).all()
-    return jsonify([dict(e) for e in results])
+    return jsonify([dict(_) for _ in results])
 
 
-@quote_bp.route("/<stock_name>&period=<period>&interval=<interval>")
-def get_historical_data(stock_name, period, interval):
-    historical_price = yahooquery.Ticker(stock_name).history(period=period, interval=interval, adj_timezone=False)
-    price_now = yahooquery.Ticker(stock_name).price[stock_name]
-    this_result = {
+@quote_bp.route("/<stock_symbol>&period=<period>&interval=<interval>")
+def get_historical_data(stock_symbol: str, period: str, interval: str) -> flask.Response:
+    """
+    Given a specific ticker, find the historical pricing of this company using the opening price.
+    Note that due to limitations with the yahoo finance library, some combinations of periods
+    and time intervals are not avaliable. \n
+    Options for periods include ['1d', '5d', '7d', '60d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max'] \n
+    Options for interval include ['1m', '2m', '5m', '15m', '30m', '60m', '90m', '1h', '1d', '5d', '1wk', '1mo', '3mo'] \n
+
+
+    :param stock_symbol: The ticker for the company
+    :type stock_symbol: str
+    :param period: Length of time
+    :type period: str
+    :param interval: Time between data points
+    :type interval: str
+    :return: A custom dictionary (refer to custom_dict) for specific key, values.
+    :rtype: dict
+    """
+    historical_price = yahooquery.Ticker(stock_symbol).history(period=period, interval=interval, adj_timezone=False)
+    price_now = yahooquery.Ticker(stock_symbol).price[stock_symbol]
+    custom_dict = {
         "priceList": [{"time": key[1], "open": value} for key, value in historical_price['open'].to_dict().items()],
         "price_now": price_now["regularMarketPrice"],
         "market_change_perc": price_now["regularMarketChangePercent"],
         "currency_symbol": price_now["currencySymbol"],
     }
-    return jsonify(this_result)
+    return jsonify(custom_dict)
