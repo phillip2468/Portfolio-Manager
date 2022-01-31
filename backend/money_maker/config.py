ab7d9f4a5f9ffe65c1e7f12370fc1e14a411880a
phillip2468 <54766922+phillip2468@users.noreply.github.com>
2022-01-31 18:57:53 +1100

    confusing logggin and logout

--- backend/money_maker/config.py
+++ backend/money_maker/config.py
@@ -1,5 +1,5 @@
 import os
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 from dotenv import load_dotenv
 
@@ -23,7 +23,7 @@ SQLALCHEMY_ENGINE_OPTIONS = {
     "executemany_values_page_size": 10000
 }
 SECRET_KEY = os.getenv("SECRET_KEY")
-SESSION_COOKIE_SECURE = True
-JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
-JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
 
+JWT_COOKIE_SECURE = True
+JWT_TOKEN_LOCATION = ["cookies"]
+JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=15)
--- frontend/src/store/StoreCredentials.jsx
+++ frontend/src/store/StoreCredentials.jsx
@@ -8,29 +8,7 @@ export const ClientContext = createContext(null);
 
 export const ClientWrapper = ({children}) => {
     const navigate = useNavigate();
-    const [loading, setLoading] = useState(true)
 
-    const [token, setToken] = useState(()=> localStorage.getItem('token') ? localStorage.getItem('token') : null);
-    const [refreshToken, setRefreshToken] = useState(()=> localStorage.getItem('refreshToken') ? localStorage.getItem('refreshToken') : null)
-
-    const replaceRefreshToken = () => {
-        console.log(refreshToken)
-        if (token) {
-            FetchFunction('GET', '/auth/refresh', refreshToken, null)
-                .then(response => {
-                    localStorage.setItem('token', response['access_token'])
-                    contextData.setToken(localStorage.getItem('token'))
-                })
-                .catch(error => {
-                    if (error !== "EarlyRefreshError") {
-                        logoutUser()
-                    }
-                })
-        }
-        if (loading) {
-            setLoading(false)
-        }
-    }
 
     const loginUser = (email, password) => {
         const body = {
@@ -39,11 +17,8 @@ export const ClientWrapper = ({children}) => {
         }
         FetchFunction('POST', 'auth/login', null, body)
             .then(response => {
-                localStorage.setItem('token', response['access_token'])
-                localStorage.setItem('refreshToken', response['refresh_token'])
-                contextData.setToken(localStorage.getItem('token'))
-                contextData.setRefreshToken(response['refresh_token'])
-                navigate('/')
+                console.log(response)
+                alert(response)
             })
             .catch(e => {
                 console.log(e)
@@ -52,32 +27,23 @@ export const ClientWrapper = ({children}) => {
     }
 
     const logoutUser = () => {
-        localStorage.removeItem('token')
-        localStorage.removeItem('refreshToken')
-        contextData.setToken(null)
-        contextData.setRefreshToken(null)
-        navigate('/login')
+        FetchFunction('POST', 'auth/logout', null, null)
+            .then(res => res.json())
+            .then(res => {
+                alert(res)
+                navigate('/login')
+            })
+            .catch(error => alert(error))
     }
 
     let contextData = {
-        token, setToken, replaceRefreshToken, loginUser, logoutUser, refreshToken, setRefreshToken
+       loginUser, logoutUser
     }
 
     useEffect( () => {
+        // eslint-disable-next-line
+    }, [])
 
-        if (loading) {
-            replaceRefreshToken();
-        }
-        // eslint-disable-next-line
-        let interval = setInterval(()=> {
-            if (contextData.token) {
-                replaceRefreshToken();
-            }
-        }, 1000 * 60 * 2)
-        return () => clearInterval(interval)
-        // eslint-disable-next-line
-    }, [contextData.token, loading])
-    
     return (
         <ClientContext.Provider value={contextData}>
             {children}
