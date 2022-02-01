10621392adba8083247dbf0f473eddc547431874
phillip2468 <54766922+phillip2468@users.noreply.github.com>
2022-02-01 20:46:56 +1100

    added some simple regex for validating emails

---
+++ frontend/src/components/EmailAddress.jsx
@@ -0,0 +1,49 @@
+import {TextField} from "@mui/material";
+import {useState} from "react";
+
+const errorText = 'Email addresses should be longer than 10 characters, contain an @ symbol and should contain a domain.'
+const defaultText = 'You can enter numbers, letters and periods'
+
+// https://stackoverflow.com/questions/52188192/what-is-the-simplest-and-shortest-way-for-validating-an-email-in-react
+function validateEmail (email) {
+    const regexp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regexp.test(email);
+}
+
+
+const EmailAddress = ({placeholder, email, setValue}) => {
+
+    const [error, setError] = useState(false);
+    const [helperText, setHelperText] = useState(defaultText);
+
+    const checkEmailAddress = (event) => {
+        const emailAddress = event.target.value;
+        if (!validateEmail(emailAddress)) {
+            setError(true)
+            setHelperText(errorText)
+        } else {
+            setError(false)
+            setHelperText(defaultText)
+        }
+    }
+
+    return (
+        <>
+            <TextField
+                variant={"outlined"}
+                placeholder={placeholder}
+                autoFocus={true}
+                fullWidth={true}
+                sx={{width: "350px"}}
+                value={email}
+                onChange={setValue}
+                error={error}
+                onBlur={checkEmailAddress}
+                onFocus={()=>setError(false)}
+                helperText={helperText}
+            />
+        </>
+    )
+}
+
+export default EmailAddress;
--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,10 +1,39 @@
-import {Grid} from "@mui/material";
+import {Grid, Typography} from "@mui/material";
+import EmailAddress from "../components/EmailAddress";
+import {useState} from "react";
 
 const RegisterPage = () => {
+    const [email, setEmail] = useState('');
+
+    const handleSearch = (event) => {
+        const typedEmail = event.target.value;
+        setEmail(typedEmail);
+    }
+
+
     return (
-        <Grid item>
-            hi there
-        </Grid>
+        <>
+            <Grid item>
+                <Grid container
+                      spacing={2}
+                      direction={"column"}
+                      alignItems={"center"}
+                      justifyContent={"center"}
+                >
+                    <Grid item>
+                        <Typography variant={"h4"}>Register account</Typography>
+                    </Grid>
+
+                    <Grid item>
+                        <EmailAddress
+                            placeholder={'Type in an email address'}
+                            email={email}
+                            setValue={handleSearch}
+                        />
+                    </Grid>
+                </Grid>
+            </Grid>
+        </>
     )
 }
 
