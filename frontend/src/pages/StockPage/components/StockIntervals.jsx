bfd4a92f328b3634d145fe868ffa83222800187f
Phillip Davis <phillipdavis2468@gmail.com>
2022-02-02 21:17:49 +1100

    fixed most of the complaints with eslint, however removed some of the functionality of the search bar

--- frontend/src/pages/StockPage/components/StockIntervals.jsx
+++ frontend/src/pages/StockPage/components/StockIntervals.jsx
@@ -1,18 +1,18 @@
-import {Grid} from "@mui/material";
-import * as PropTypes from "prop-types";
-import React from "react";
+import { Grid } from '@mui/material'
+import * as PropTypes from 'prop-types'
+import React from 'react'
 
 const StockIntervals = (props) => {
-    return <Grid item>
+  return <Grid item>
         <Grid container>
             {props.listOfIntervals.map(props.callbackfn)}
         </Grid>
-    </Grid>;
+    </Grid>
 }
 
 StockIntervals.propTypes = {
-    listOfIntervals: PropTypes.arrayOf(PropTypes.string),
-    callbackfn: PropTypes.func
-};
+  listOfIntervals: PropTypes.arrayOf(PropTypes.string),
+  callbackfn: PropTypes.func
+}
 
-export default StockIntervals;
+export default StockIntervals
--- frontend/src/store/StoreCredentials.jsx
+++ frontend/src/store/StoreCredentials.jsx
@@ -1,64 +1,67 @@
-import {createContext, useEffect, useState} from "react";
-import {FetchFunction} from "../components/FetchFunction";
-import {useNavigate} from "react-router-dom";
+import { createContext, useEffect, useState } from 'react'
+import { FetchFunction } from '../components/FetchFunction'
+import { useNavigate } from 'react-router-dom'
+import * as PropTypes from 'prop-types'
 
 // https://www.youtube.com/watch?v=xjMP0hspNLE
 
-export const ClientContext = createContext(null);
+export const ClientContext = createContext(null)
 
-export const ClientWrapper = ({children}) => {
-    const navigate = useNavigate();
-    const [loggedIn, setLoggedIn] = useState(false)
+export const ClientWrapper = ({ children }) => {
+  const navigate = useNavigate()
+  const [loggedIn, setLoggedIn] = useState(false)
 
-    const loginUser = (email, password) => {
-        const body = {
-            email: email,
-            password: password,
-        }
-        FetchFunction('POST', 'auth/login', body)
-            .then(() => {
-                setLoggedIn(true)
-                navigate('/')
-            })
-            .catch(e => {
-                console.log(e)
-            })
+  const loginUser = (email, password) => {
+    const body = {
+      email: email,
+      password: password
     }
+    FetchFunction('POST', 'auth/login', body)
+      .then(() => {
+        setLoggedIn(true)
+        navigate('/')
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }
 
+  const logoutUser = () => {
+    FetchFunction('POST', 'auth/logout', null)
+      .then(res => {
+        console.log(res)
+        setLoggedIn(false)
+        navigate('/login')
+      })
+      .catch(res => console.log(res))
+  }
 
-    const logoutUser = () => {
-        FetchFunction('POST', 'auth/logout', null)
-            .then(res => {
-                console.log(res)
-                setLoggedIn(false)
-                navigate('/login')
-            })
-            .catch(res => console.log(res))
-    }
+  const findUser = () => {
+    FetchFunction('GET', 'auth/which_user', null)
+      .then(() => {
+        setLoggedIn(true)
+      })
+      .catch(() => {
+        setLoggedIn(false)
+      })
+  }
 
-    const findUser = () => {
-        FetchFunction('GET', 'auth/which_user', null)
-            .then(() => {
-                setLoggedIn(true)
-            })
-            .catch(()=> {
-                setLoggedIn(false)
-            })
-    }
+  const contextData = {
+    loginUser, logoutUser, findUser, loggedIn, setLoggedIn
+  }
 
-
-    let contextData = {
-       loginUser, logoutUser, findUser, loggedIn, setLoggedIn
-    }
-
-    useEffect( () => {
-        // eslint-disable-next-line
+  useEffect(() => {
+    // eslint-disable-next-line
         findUser()
-    }, [loggedIn])
+  }, [loggedIn])
 
-    return (
+  return (
         <ClientContext.Provider value={contextData}>
             {children}
         </ClientContext.Provider>
-    )
+  )
+}
+
+ClientWrapper.propTypes = {
+  children: PropTypes.any
 }
