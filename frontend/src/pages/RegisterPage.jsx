86f60336371b67baa290f499ff80c5b039984024
phillip2468 <54766922+phillip2468@users.noreply.github.com>
2022-02-03 16:34:22 +1100

    addded some quick proptypes

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -37,7 +37,7 @@ const RegisterPage = () => {
     }
     FetchFunction('POST', 'auth/register', body)
       .then(() => navigate('/'))
-      .catch(error => console.log(error))
+      .catch(error => alert(error))
   }
 
   useEffect(() => {
