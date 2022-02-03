8b8e60fa20ef2e6a0d8c0bf96f7cfb651841121d
phillip2468 <54766922+phillip2468@users.noreply.github.com>
2022-02-03 20:48:41 +1100

    need a way to add composite keys

--- backend/money_maker/models/portfolio.py
+++ backend/money_maker/models/portfolio.py
@@ -1,6 +1,7 @@
 from flask_marshmallow import Schema
 from money_maker.extensions import db, marshmallow
-from sqlalchemy import TIMESTAMP, Column, ForeignKey, Integer, String, func
+from sqlalchemy import (TIMESTAMP, Column, ForeignKey, ForeignKeyConstraint,
+                        Integer, String, func)
 
 
 class Portfolio(db.Model):
@@ -12,8 +13,13 @@ class Portfolio(db.Model):
     portfolio_id = Column(Integer, primary_key=True, autoincrement=True)
     portfolio_name = Column(String(length=100), unique=True)
     user_id = Column(Integer, ForeignKey('user.user_id'))
-    stock_id = Column(Integer)
+    stock_id = Column(Integer, ForeignKey('ticker_prices.stock_id'))
     last_inserted = Column(TIMESTAMP, server_default=func.now(), server_onupdate=func.utc_timestamp())
+    ___table_args__ = (
+        ForeignKeyConstraint(columns=["portfolio_name", "user_id", "stock_id"],
+                             refcolumns=["portfolio_name", "user.user_id", "ticker_price.stock_id"],
+                             onupdate="CASCADE", ondelete="CASCADE",), {}
+    )
 
 
 class PortfolioSchema(marshmallow.SQLAlchemyAutoSchema):
