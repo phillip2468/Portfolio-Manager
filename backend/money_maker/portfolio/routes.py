c5a876002ab881086f223ee4a820c4e0f8ddac20
Phillip Davis <phillipdavis2468@gmail.com>
2022-02-04 15:38:03 +1100

    added a few routes for portfolio

--- backend/money_maker/portfolio/routes.py
+++ backend/money_maker/portfolio/routes.py
@@ -1,11 +1,31 @@
 from flask import Blueprint
 from money_maker.extensions import db
-from money_maker.models.portfolio import Portfolio, portfolio_schema
+from money_maker.models.portfolio import Portfolio as pF, portfolio_schema
 
 portfolio_bp = Blueprint("portfolio_bp", __name__, url_prefix="/portfolio")
 
 
 @portfolio_bp.route("<user_id>", methods=["GET"])
-def get_portfolio(user_id):
-    results = db.session.query(Portfolio).filter(Portfolio.user_id == user_id).all()
+def get_portfolio_names(user_id: int):
+    """
+    Returns all the portfolio names that a particular user
+    has.
+    :param user_id: The user id
+    :return: flask.Response
+    """
+    results = db.session.query(pF.portfolio_name).filter(pF.user_id == user_id).all()
+    return portfolio_schema.jsonify(results, many=True)
+
+
+@portfolio_bp.route("<user_id>/<portfolio_name>", methods=["GET"])
+def get_portfolio(user_id: int, portfolio_name: str):
+    """
+    Returns a stocks list by portfolio name.
+
+    :param user_id: The user id
+    :param portfolio_name: The portfolio name
+    :return: flask.Response
+    """
+    results = db.session.query(pF.portfolio_name, pF.stock_id)\
+        .filter(pF.user_id == user_id, pF.portfolio_name == portfolio_name).all()
     return portfolio_schema.jsonify(results, many=True)
