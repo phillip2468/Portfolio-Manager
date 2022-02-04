9100bde62daacb7e2fc9ebb2874c565db77b3969
Phillip Davis <phillipdavis2468@gmail.com>
2022-02-04 15:48:09 +1100

    added a quick exception handler for duplicate entries

--- backend/money_maker/portfolio/routes.py
+++ backend/money_maker/portfolio/routes.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from sqlalchemy.exc import IntegrityError
+
 from money_maker.extensions import db
 from money_maker.models.portfolio import Portfolio as pF, portfolio_schema
 
@@ -6,7 +8,7 @@ portfolio_bp = Blueprint("portfolio_bp", __name__, url_prefix="/portfolio")
 
 
 @portfolio_bp.route("<user_id>", methods=["GET"])
-def get_portfolio_names(user_id: int):
+def get_portfolio_names_by_user(user_id: int):
     """
     Returns all the portfolio names that a particular user
     has.
@@ -18,7 +20,7 @@ def get_portfolio_names(user_id: int):
 
 
 @portfolio_bp.route("<user_id>/<portfolio_name>", methods=["GET"])
-def get_portfolio(user_id: int, portfolio_name: str):
+def get_portfolio_stocks_by_user(user_id: int, portfolio_name: str):
     """
     Returns a stocks list by portfolio name.
 
@@ -26,6 +28,29 @@ def get_portfolio(user_id: int, portfolio_name: str):
     :param portfolio_name: The portfolio name
     :return: flask.Response
     """
-    results = db.session.query(pF.portfolio_name, pF.stock_id)\
+    results = db.session.query(pF.portfolio_name, pF.stock_id) \
         .filter(pF.user_id == user_id, pF.portfolio_name == portfolio_name).all()
     return portfolio_schema.jsonify(results, many=True)
+
+
+@portfolio_bp.route("<user_id>/<portfolio_name>/<stock_id>", methods=["POST"])
+def add_stock_to_portfolio(user_id: int, portfolio_name: str, stock_id: int):
+    """
+    Add a stock to a particular portfolio, using their stock_id from the database.
+    Returns a message indicating user success.
+
+    :param user_id: The user id
+    :param portfolio_name: The portfolio name
+    :param stock_id: The stock id
+    :return: flask.Response
+    """
+    stock = pF(stock_id=stock_id, portfolio_name=portfolio_name, user_id=user_id)
+    db.session.add(stock)
+    db.session.commit()
+
+    return jsonify({"msg", "Successfully added stock"}), 200
+
+
+@portfolio_bp.errorhandler(IntegrityError)
+def exception_handler(e):
+    return jsonify({'message': e.description}), 400
