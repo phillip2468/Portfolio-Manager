864b36d4538ef8708d24982c761886f11830da58
Phillip Davis <phillipdavis2468@gmail.com>
2022-02-06 14:46:00 +1100

    can now delete stocks from portfolio

--- backend/money_maker/portfolio/routes.py
+++ backend/money_maker/portfolio/routes.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, jsonify, request
+from sqlalchemy.exc import IntegrityError
+
 from money_maker.extensions import db
 from money_maker.models.portfolio import Portfolio as pF
 from money_maker.models.portfolio import portfolio_schema
 from money_maker.models.ticker_prices import TickerPrice as tP
-from sqlalchemy import distinct
-from sqlalchemy.exc import IntegrityError
 
 portfolio_bp = Blueprint("portfolio_bp", __name__, url_prefix="/portfolio")
 
@@ -67,6 +67,22 @@ def add_stock_to_portfolio(user_id: int, portfolio_name: str, stock_id: int):
     return jsonify({"msg", "Successfully added stock"}), 200
 
 
+@portfolio_bp.route("<user_id>/<portfolio_name>/<stock_id>", methods=["DELETE"])
+def remove_stock_from_portfolio(user_id: int, portfolio_name: str, stock_id: int):
+    """
+    Removes a particular portfolio, using their stock_id from the database.
+    Returns a message indicating user success.
+
+    :param user_id: The user id
+    :param portfolio_name: The portfolio name
+    :param stock_id: The stock id
+    :return: flask.Response
+    """
+    db.session.query(pF).filter(pF.stock_id == stock_id).delete()
+
+    return jsonify({"msg": "Successfully deleted the stock"}), 200
+
+
 @portfolio_bp.route("<user_id>/<portfolio_name>/<stock_id>", methods=["PATCH"])
 def update_stock_in_portfolio(user_id: int, portfolio_name: str, stock_id: int):
     """
@@ -82,11 +98,11 @@ def update_stock_in_portfolio(user_id: int, portfolio_name: str, stock_id: int):
     units_price = req.get("units_price", None)
     units_purchased = req.get("units_purchased", None)
 
-    db.session.query(pF).filter(pF.user_id == user_id, pF.portfolio_name == portfolio_name, pF.stock_id == stock_id)\
+    db.session.query(pF).filter(pF.user_id == user_id, pF.portfolio_name == portfolio_name, pF.stock_id == stock_id) \
         .update(values={"units_price": units_price, "units_purchased": units_purchased}, synchronize_session="fetch")
     db.session.commit()
 
-    return jsonify({"msg" : "Successfully updated the stock"}), 200
+    return jsonify({"msg": "Successfully updated the stock"}), 200
 
 
 @portfolio_bp.errorhandler(IntegrityError)
