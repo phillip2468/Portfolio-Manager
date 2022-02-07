654c665447a39078672b73dfae424a398f4d8bf1
Phillip Davis <phillipdavis2468@gmail.com>
2022-02-07 14:53:44 +1100

    added the button for the watchlist

--- frontend/src/pages/WatchListPage/WatchListPage.jsx
+++ frontend/src/pages/WatchListPage/WatchListPage.jsx
@@ -1,6 +1,58 @@
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Grid,
+  TextField,
+  Typography
+} from '@mui/material'
+import Button from '@mui/material/Button'
+import { useState } from 'react'
+
 const WatchListPage = () => {
+  const [openWLDialog, setOpenWLDialog] = useState(false)
+
   return (
     <>
+      <Grid item>
+        <Typography align={'center'} variant={'h5'}>
+          Watchlist page
+        </Typography>
+      </Grid>
+
+      <Grid item>
+        <Grid container justifyContent={'center'}>
+          <Button onClick={() => setOpenWLDialog(true)} variant={'contained'}>
+            Create a new watchlist
+          </Button>
+        </Grid>
+      </Grid>
+
+      <Dialog onClose={() => setOpenWLDialog(false)} open={openWLDialog}>
+        <DialogTitle>
+          Create a new watchlist
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Enter a title for your watchlist here
+          </DialogContentText>
+
+          <TextField autoFocus
+                     fullWidth
+                     id={'watchlist_name'}
+                     label={'Watchlist name'}
+                     margin={'dense'}
+                     type={'text'}
+                     variant={'standard'}/>
+
+          <DialogActions>
+            <Button onClick={() => setOpenWLDialog(false)}>Cancel</Button>
+            <Button onClick={() => setOpenWLDialog(false)}>Add</Button>
+          </DialogActions>
+        </DialogContent>
+      </Dialog>
     </>
   )
 }
