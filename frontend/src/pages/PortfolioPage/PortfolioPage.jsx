f7b5748d088c2b12eb19ea6a03df81de9ebc9250
Phillip Davis <phillipdavis2468@gmail.com>
2022-02-07 13:57:32 +1100

    extracted the entirety of columns from the portfolio into its own file

--- frontend/src/pages/PortfolioPage/PortfolioPage.jsx
+++ frontend/src/pages/PortfolioPage/PortfolioPage.jsx
@@ -10,17 +10,13 @@ import {
   TextField,
   Typography
 } from '@mui/material'
-import DataTable from 'react-data-table-component'
 import { useCallback, useContext, useEffect, useMemo, useState } from 'react'
-import { ClientContext } from '../store/StoreCredentials'
-import { FetchFunction } from '../components/FetchFunction'
+import { ClientContext } from '../../store/StoreCredentials'
+import { FetchFunction } from '../../components/FetchFunction'
 import Button from '@mui/material/Button'
-import AddStockDialog from '../components/AddStockDialog'
-import { Link } from 'react-router-dom'
-
-function findById (array, id) {
-  return array.findIndex((d) => d.portfolio_id === id)
-}
+import AddStockDialog from '../../components/AddStockDialog'
+import TableOfStocks from '../../components/TableOfStocks'
+import Columns from './components/Columns'
 
 const PortfolioPage = () => {
   const { userId } = useContext(ClientContext)
@@ -39,116 +35,12 @@ const PortfolioPage = () => {
 
   const [stockDialogOpen, setStockDialogOpen] = useState(false)
 
-  useEffect(() => {
-    if (userId !== null) {
-      FetchFunction('GET', `portfolio/${userId}`, null)
-        .then(res => setListOfPortfolios(res))
-        .catch(res => console.log(res))
-    }
-  }, [userId])
-
   const getStocksFromPortfolio = (portfolioName) => {
     FetchFunction('GET', `portfolio/${userId}/${portfolioName}`)
       .then(res => setListOfStocks(res))
       .catch(error => console.log(error))
   }
-
-  useEffect(() => {
-    if (selectedPortfolio !== '') {
-      getStocksFromPortfolio(selectedPortfolio)
-    }
-  }, [selectedPortfolio, stockDialogOpen])
-
-  const columns = useMemo(() => {
-    const handleCellEditable = (field) => (row) => (e) => {
-      if (e.target.value >= 0) {
-        const newRow = { ...row }
-        newRow[field] = e.target.value
-
-        const newData = listOfStocks.slice(0)
-        newData[findById(listOfStocks, row.portfolio_id)] = newRow
-        setListOfStocks(newData)
-
-        const body = {
-          units_price: newRow.units_price,
-          units_purchased: newRow.units_purchased
-        }
-        FetchFunction('PATCH', `portfolio/${userId}/${newRow.portfolio_name}/${newRow.stock_details.stock_id}`, body)
-          .then(res => console.log(res))
-          .catch(error => alert(error))
-      }
-    }
-
-    return [
-      {
-        name: 'symbol',
-        selector: row => row.stock_details.symbol,
-        sortable: true,
-        cell: row => <Link target="_blank" to={`/${row.stock_details.symbol}`}>{row.stock_details.symbol}</Link>
-      },
-      {
-        name: 'exchange',
-        selector: row => row.stock_details.exchange,
-        sortable: true
-      },
-      {
-        name: 'Name',
-        selector: row => row.stock_details.stock_name,
-        wrap: true,
-        sortable: true
-      },
-      {
-        name: 'Last',
-        selector: row => row.stock_details.market_current_price,
-        sortable: true
-      },
-      {
-        name: 'Currency',
-        selector: row => row.stock_details.currency,
-        sortable: true
-      },
-      {
-        name: 'Todays change %',
-        selector: row => row.stock_details.market_change_percentage,
-        sortable: true,
-        format: row => (row.stock_details.market_change_percentage * 100).toFixed(2)
-      },
-      {
-        name: 'Unit price',
-        selector: row => row.units_price,
-        sortable: true,
-        cell: (row) => (
-          <TextField
-            onChange={handleCellEditable('units_price')(row)}
-            value={row.units_price}
-          />
-        )
-      },
-      {
-        name: 'Units',
-        selector: row => row.units_purchased,
-        sortable: true,
-        cell: (row) => (
-          <TextField
-               onChange={handleCellEditable('units_purchased')(row)}
-               value={row.units_purchased}
-               />
-        )
-      },
-      {
-        name: 'Value',
-        selector: row => (row.units_purchased * row.units_price),
-        sortable: true,
-        format: row => ((parseFloat(row.units_purchased) * row.units_price).toFixed(2))
-      },
-      {
-        name: 'Change %',
-        selector: row => (100 - ((row.stock_details.market_current_price / row.units_price) * 100)),
-        sortable: true,
-        format: row => (100 - ((row.stock_details.market_current_price / row.units_price) * 100)).toFixed(2)
-      }
-    ]
-  }, [listOfStocks])
+  const columns = Columns(listOfStocks, setListOfStocks, userId)
 
   const handleRowsSelected = useCallback(state => {
     setSelectedRows(state.selectedRows)
@@ -184,6 +76,20 @@ const PortfolioPage = () => {
       )
     }
   }
+
+  useEffect(() => {
+    if (userId !== null) {
+      FetchFunction('GET', `portfolio/${userId}`, null)
+        .then(res => setListOfPortfolios(res))
+        .catch(res => console.log(res))
+    }
+  }, [userId])
+
+  useEffect(() => {
+    if (selectedPortfolio !== '') {
+      getStocksFromPortfolio(selectedPortfolio)
+    }
+  }, [selectedPortfolio, stockDialogOpen])
 
   // noinspection JSValidateTypes
   return (
@@ -262,21 +168,9 @@ const PortfolioPage = () => {
       </Grid>
 
       <Grid item>
-        <DataTable
-          actions={renderAddStock()}
-          clearSelectedRows={toggleCleared}
-          columns={columns}
-          contextActions={contextActions}
-          data={listOfStocks}
-          highlightOnHover={true}
-          onSelectedRowsChange={handleRowsSelected}
-          pagination={true}
-          pointerOnHover={true}
-          selectableRows={true}
-          subHeaderAlign={'center'}
-          theme={'dark'}
-          title={`${selectedPortfolio} portfolio's`}
-        />
+        <TableOfStocks actions={renderAddStock()} clearSelectedRows={toggleCleared} columns={columns}
+                       contextActions={contextActions} data={listOfStocks} onSelectedRowsChange={handleRowsSelected}
+                       selectedPortfolio={selectedPortfolio}/>
       </Grid>
     </>
   )
