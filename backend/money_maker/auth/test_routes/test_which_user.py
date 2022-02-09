e2a598c796592b24fc1a183e4f79462fb0a6ab34
phillip2468 <54766922+phillip2468@users.noreply.github.com>
2022-02-09 19:08:28 +1100

    added a quick test for invalid which_user

--- backend/money_maker/auth/test_routes/test_which_user.py
+++ backend/money_maker/auth/test_routes/test_which_user.py
@@ -6,8 +6,9 @@ from conftest import HTTP_SUCCESS_CODE, NUMBER_OF_USERS, REPEAT_TESTS
 @pytest.mark.repeat(REPEAT_TESTS)
 def test_which_user(client, client_accounts) -> None:
     """
-    Tests that finding a particular user id in the database.
-    Useful for identifying other features that are related
+    Tests that the system can find the user using cookies from the
+    request. Returns a dictionary containing a user_id.
+    Useful for identifying other features that are related to a user
     in the database.
 
     :param client: The flask app
@@ -21,3 +22,22 @@ def test_which_user(client, client_accounts) -> None:
 
         response: flask.Response = client.get("/auth/which_user")
         assert response.status_code == HTTP_SUCCESS_CODE
+        assert response.get_json()["user_id"] == i + 1
+
+
+@pytest.mark.repeat(REPEAT_TESTS)
+def test_which_user_invalid(client, client_accounts) -> None:
+    """
+    Tests unauthorized route status by clearing cookies of client
+    response.
+
+    :param client: The flask app
+    :type client: flask.testing.FlaskClient
+    :param client_accounts: The dictionary of users
+    :type client_accounts: dict
+    """
+    client.cookie_jar.clear()
+    response: flask.Response = client.get("/auth/which_user")
+    assert response.status_code != HTTP_SUCCESS_CODE
+
+
