fa29dbf53b9d1c0a726c5204184e7bc4a24b3aa3
Phillip Davis <phillipdavis2468@gmail.com>
2022-02-10 22:51:19 +1100

    can now test for stocks to be inserted

--- backend/money_maker/tasks/tests/test_task.py
+++ backend/money_maker/tasks/tests/test_task.py
@@ -3,7 +3,7 @@ from money_maker.extensions import db
 from money_maker.models.ticker_prices import TickerPrice
 
 
-def test_get_stock_information(client):
+def test_get_stock_information(client, symbols):
     response = client.get("/task")
     assert response.status_code == HTTP_SUCCESS_CODE
     assert len(db.session.query(TickerPrice).all()) == 1
