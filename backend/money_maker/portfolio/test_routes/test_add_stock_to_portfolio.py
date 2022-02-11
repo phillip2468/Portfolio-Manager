ec4c919320663b3046d2f44395d6f4a44035b582
Phillip Davis <phillipdavis2468@gmail.com>
2022-02-11 21:51:57 +1100

    added some better docstrings to test add stock to portfolio

--- backend/money_maker/portfolio/test_routes/test_add_stock_to_portfolio.py
+++ backend/money_maker/portfolio/test_routes/test_add_stock_to_portfolio.py
@@ -1,19 +1,22 @@
+from flask.testing import FlaskClient
+
 from conftest import HTTP_SUCCESS_CODE
 from money_maker.extensions import db, faker_data
 from money_maker.models.portfolio import Portfolio
 
 
-def test_add_stock_to_portfolio(client, client_accounts, symbols, stock_prices, logged_in_user_id, sample_portfolio) -> None:
+def test_add_stock_to_portfolio(client: FlaskClient, client_accounts: list[dict], symbols: None,
+                                stock_prices: None, logged_in_user_id: int, sample_portfolio: str) -> None:
     """
     GIVEN a stock id
     WHEN a user is logged in and wants to add a stock to portfolio
     THEN check that the new stock has been added
     Args:
-        client: The flask app
-        client_accounts: List of dict
-        symbols: Places symbols into database TickerPrice
-        stock_prices: Places stock prices into database TickerPrice
-        logged_in_user_id: The user id logged in
+        client: The flask application
+        client_accounts: The list of dictionaries of clients
+        symbols: The query to create stock symbol rows
+        stock_prices: The query to update up to 100 stocks of alphabetically
+        logged_in_user_id: The logged-in user_id
         sample_portfolio: Places sample portfolio into database portfolio
     """
 
@@ -24,17 +27,18 @@ def test_add_stock_to_portfolio(client, client_accounts, symbols, stock_prices, 
                .filter(Portfolio.user_id == logged_in_user_id, Portfolio.stock_id == 1).all()) == 1
 
 
-def test_add_multiple_stocks_to_portfolio(client, client_accounts, symbols, stock_prices, logged_in_user_id, sample_portfolio) -> None:
+def test_add_multiple_stocks_to_portfolio(client: FlaskClient, client_accounts: list[dict], symbols: None,
+                                          stock_prices: None, logged_in_user_id: int, sample_portfolio: str) -> None:
     """
     GIVEN multiple stock_ids
     WHEN a user is logged in and wants to add a stock to portfolio
     THEN check that the new stock has been added
     Args:
-        client: The flask app
-        client_accounts: List of dict
-        symbols: Places symbols into database TickerPrice
-        stock_prices: Places stock prices into database TickerPrice
-        logged_in_user_id: The user id logged in
+        client: The flask application
+        client_accounts: The list of dictionaries of clients
+        symbols: The query to create stock symbol rows
+        stock_prices: The query to update up to 100 stocks of alphabetically
+        logged_in_user_id: The logged-in user_id
         sample_portfolio: Places sample portfolio into database portfolio
     """
     response = client.post(f"""/portfolio/{logged_in_user_id}/{sample_portfolio}""")
@@ -47,17 +51,18 @@ def test_add_multiple_stocks_to_portfolio(client, client_accounts, symbols, stoc
     assert len(db.session.query(Portfolio).filter(Portfolio.user_id == logged_in_user_id).all()) == 10
 
 
-def test_add_multiple_stocks_to_different_portfolios(client, client_accounts, symbols, stock_prices, logged_in_user_id, sample_portfolio) -> None:
+def test_add_multiple_stocks_to_different_portfolios(client: FlaskClient, client_accounts: list[dict], symbols: None,
+                                                     stock_prices: None, logged_in_user_id: int, sample_portfolio: str) -> None:
     """
     GIVEN multiple stock_ids
     WHEN a user is logged in and wants to add a stock to a particular portfolio (and already has multiple portfolios)
     THEN check that the new stocks corresponds to the sample portfolio
     Args:
-        client: The flask app
-        client_accounts: List of dict
-        symbols: Places symbols into database TickerPrice
-        stock_prices: Places stock prices into database TickerPrice
-        logged_in_user_id: The user id logged in
+        client: The flask application
+        client_accounts: The list of dictionaries of clients
+        symbols: The query to create stock symbol rows
+        stock_prices: The query to update up to 100 stocks of alphabetically
+        logged_in_user_id: The logged-in user_id
         sample_portfolio: Places sample portfolio into database portfolio
     """
     response = client.post(f"""/portfolio/{logged_in_user_id}/{sample_portfolio}""")
@@ -74,18 +79,19 @@ def test_add_multiple_stocks_to_different_portfolios(client, client_accounts, sy
                                                   Portfolio.portfolio_name == sample_portfolio).all()) == 10
 
 
-def test_add_invalid_stock_to_portfolio(client, client_accounts, symbols, stock_prices, logged_in_user_id, sample_portfolio) -> None:
+def test_add_invalid_stock_to_portfolio(client: FlaskClient, client_accounts: list[dict], symbols: None,
+                                        stock_prices: None, logged_in_user_id: int, sample_portfolio: str) -> None:
     """
     GIVEN an invalid stock_id
     WHEN a user is logged in and wants to add a stock to a particular portfolio
     THEN check that the new stock HAS NOT been added
 
     Args:
-        client: The flask app
-        client_accounts: List of dict
-        symbols: Places symbols into database TickerPrice
-        stock_prices: Places stock prices into database TickerPrice
-        logged_in_user_id: The user id logged in
+        client: The flask application
+        client_accounts: The list of dictionaries of clients
+        symbols: The query to create stock symbol rows
+        stock_prices: The query to update up to 100 stocks of alphabetically
+        logged_in_user_id: The logged-in user_id
         sample_portfolio: Places sample portfolio into database portfolio
     """
     response = client.post(f"""/portfolio/{logged_in_user_id}/{sample_portfolio}/-{faker_data.random_number()}""")
