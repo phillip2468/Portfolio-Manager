a913d6e11f9a00d4629adf6e3c08cc61e8201bda
Phillip Davis <phillipdavis2468@gmail.com>
2022-02-11 21:32:01 +1100

    added some more helpful docstrings in the google format

--- backend/money_maker/portfolio/test_routes/test_add_stock_to_portfolio.py
+++ backend/money_maker/portfolio/test_routes/test_add_stock_to_portfolio.py
@@ -1,117 +1,97 @@
 from conftest import HTTP_SUCCESS_CODE
-from money_maker.extensions import db
+from money_maker.extensions import db, faker_data
 from money_maker.models.portfolio import Portfolio
 
 
-def test_add_stock_to_portfolio(client, client_accounts, symbols, stock_prices, logged_in_user_id):
+def test_add_stock_to_portfolio(client, client_accounts, symbols, stock_prices, logged_in_user_id, sample_portfolio):
     """
     GIVEN a stock id
     WHEN a user is logged in and wants to add a stock to portfolio
     THEN check that the new stock has been added
-
-    :param client: The flask testing client
-    :type client: FlaskClient
-    :param client_accounts: The users in the database
-    :type client_accounts: dict
-    :param symbols: The stock ticker symbols
-    :type symbols: Any
-    :param stock_prices: A fixture which updates all the stock prices from the symbol's fixture
-    :type stock_prices: Any
-    :param logged_in_user_id: The id of the user
-    :type logged_in_user_id: int
+    Args:
+        client: The flask app
+        client_accounts: List of dict
+        symbols: Places symbols into database TickerPrice
+        stock_prices: Places stock prices into database TickerPrice
+        logged_in_user_id: The user id logged in
+        sample_portfolio: Places sample portfolio into database portfolio
     """
 
-    response = client.post(f"""/portfolio/{logged_in_user_id}/sample_portfolio""")
-    assert response.status_code == HTTP_SUCCESS_CODE
-
-    response = client.post(f"""/portfolio/{logged_in_user_id}/sample_portfolio/1""")
+    response = client.post(f"""/portfolio/{logged_in_user_id}/{sample_portfolio}/1""")
     assert response.status_code == HTTP_SUCCESS_CODE
 
     assert len(db.session.query(Portfolio)
                .filter(Portfolio.user_id == logged_in_user_id, Portfolio.stock_id == 1).all()) == 1
 
 
-def test_add_multiple_stocks_to_portfolio(client, client_accounts, symbols, stock_prices, logged_in_user_id):
+def test_add_multiple_stocks_to_portfolio(client, client_accounts, symbols, stock_prices, logged_in_user_id, sample_portfolio):
     """
     GIVEN multiple stock_ids
     WHEN a user is logged in and wants to add a stock to portfolio
     THEN check that the new stock has been added
-
-    :param client: The flask testing client
-    :type client: FlaskClient
-    :param client_accounts: The users in the database
-    :type client_accounts: dict
-    :param symbols: The stock ticker symbols
-    :type symbols: Any
-    :param stock_prices: A fixture which updates all the stock prices from the symbol's fixture
-    :type stock_prices: Any
-    :param logged_in_user_id: The id of the user
-    :type logged_in_user_id: int
+    Args:
+        client: The flask app
+        client_accounts: List of dict
+        symbols: Places symbols into database TickerPrice
+        stock_prices: Places stock prices into database TickerPrice
+        logged_in_user_id: The user id logged in
+        sample_portfolio: Places sample portfolio into database portfolio
     """
-    response = client.post(f"""/portfolio/{logged_in_user_id}/sample_portfolio""")
+    response = client.post(f"""/portfolio/{logged_in_user_id}/{sample_portfolio}""")
     assert response.status_code == HTTP_SUCCESS_CODE
 
     for i in range(1, 9):
-        response = client.post(f"""/portfolio/{logged_in_user_id}/sample_portfolio/{i}""")
+        response = client.post(f"""/portfolio/{logged_in_user_id}/{sample_portfolio}/{i}""")
         assert response.status_code == HTTP_SUCCESS_CODE
 
-    assert len(db.session.query(Portfolio).filter(Portfolio.user_id == logged_in_user_id).all()) == 9
+    assert len(db.session.query(Portfolio).filter(Portfolio.user_id == logged_in_user_id).all()) == 10
 
 
-def test_add_multiple_stocks_to_different_portfolios(client, client_accounts, symbols, stock_prices, logged_in_user_id):
+def test_add_multiple_stocks_to_different_portfolios(client, client_accounts, symbols, stock_prices, logged_in_user_id, sample_portfolio):
     """
     GIVEN multiple stock_ids
     WHEN a user is logged in and wants to add a stock to a particular portfolio (and already has multiple portfolios)
-    THEN check that the new stock has been added to the right portfolio
-
-    :param client: The flask testing client
-    :type client: FlaskClient
-    :param client_accounts: The users in the database
-    :type client_accounts: dict
-    :param symbols: The stock ticker symbols
-    :type symbols: Any
-    :param stock_prices: A fixture which updates all the stock prices from the symbol's fixture
-    :type stock_prices: Any
-    :param logged_in_user_id: The id of the user
-    :type logged_in_user_id: int
+    THEN check that the new stocks corresponds to the sample portfolio
+    Args:
+        client: The flask app
+        client_accounts: List of dict
+        symbols: Places symbols into database TickerPrice
+        stock_prices: Places stock prices into database TickerPrice
+        logged_in_user_id: The user id logged in
+        sample_portfolio: Places sample portfolio into database portfolio
     """
-
-    response = client.post(f"""/portfolio/{logged_in_user_id}/sample_portfolio""")
+    response = client.post(f"""/portfolio/{logged_in_user_id}/{sample_portfolio}""")
     assert response.status_code == HTTP_SUCCESS_CODE
 
     response = client.post(f"""/portfolio/{logged_in_user_id}/sample_portfolio_2""")
     assert response.status_code == HTTP_SUCCESS_CODE
 
     for i in range(1, 9):
-        response = client.post(f"""/portfolio/{logged_in_user_id}/sample_portfolio/{i}""")
+        response = client.post(f"""/portfolio/{logged_in_user_id}/{sample_portfolio}/{i}""")
         assert response.status_code == HTTP_SUCCESS_CODE
 
-    assert len(db.session.query(Portfolio).filter(Portfolio.user_id == logged_in_user_id).all()) == 10
+    assert len(db.session.query(Portfolio).filter(Portfolio.user_id == logged_in_user_id,
+                                                  Portfolio.portfolio_name == sample_portfolio).all()) == 10
 
 
-def test_add_invalid_stock_to_portfolio(client, client_accounts, symbols, stock_prices, logged_in_user_id):
+def test_add_invalid_stock_to_portfolio(client, client_accounts, symbols, stock_prices, logged_in_user_id, sample_portfolio):
     """
     GIVEN an invalid stock_id
     WHEN a user is logged in and wants to add a stock to a particular portfolio
-    THEN check that the new stock has not been added
+    THEN check that the new stock HAS NOT been added
 
-    :param client: The flask testing client
-    :type client: FlaskClient
-    :param client_accounts: The users in the database
-    :type client_accounts: dict
-    :param symbols: The stock ticker symbols
-    :type symbols: Any
-    :param stock_prices: A fixture which updates all the stock prices from the symbol's fixture
-    :type stock_prices: Any
-    :param logged_in_user_id: The id of the user
-    :type logged_in_user_id: int
+    Args:
+        client: The flask app
+        client_accounts: List of dict
+        symbols: Places symbols into database TickerPrice
+        stock_prices: Places stock prices into database TickerPrice
+        logged_in_user_id: The user id logged in
+        sample_portfolio: Places sample portfolio into database portfolio
     """
-
-    response = client.post(f"""/portfolio/{logged_in_user_id}/sample_portfolio""")
-    assert response.status_code == HTTP_SUCCESS_CODE
-
-    response = client.post(f"""/portfolio/{logged_in_user_id}/sample_portfolio/-100""")
+    response = client.post(f"""/portfolio/{logged_in_user_id}/{sample_portfolio}/-{faker_data.random_number()}""")
     assert response.status_code != HTTP_SUCCESS_CODE
 
+    # Remember that when a portfolio is created, no stocks are added (which still means there's the original row)
+    # so we must count it
     assert len(db.session.query(Portfolio.stock_id)
-               .filter(Portfolio.user_id == logged_in_user_id, Portfolio.stock_id == -100).all()) == 0
+               .filter(Portfolio.user_id == logged_in_user_id).all()) == 1
