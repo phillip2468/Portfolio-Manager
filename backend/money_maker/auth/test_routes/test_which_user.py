799745817f17f40888f4d319fbc7f23ba916af53
Phillip Davis <phillipdavis2468@gmail.com>
2022-02-11 22:04:00 +1100

    added type hints to which_user

--- backend/money_maker/auth/test_routes/test_which_user.py
+++ backend/money_maker/auth/test_routes/test_which_user.py
@@ -1,43 +1,44 @@
-import flask
 import pytest
+from flask.testing import FlaskClient
+
 from conftest import HTTP_SUCCESS_CODE, NUMBER_OF_USERS, REPEAT_TESTS
 
 
 @pytest.mark.repeat(REPEAT_TESTS)
-def test_which_user(client, client_accounts) -> None:
+def test_which_user(client: FlaskClient, client_accounts: list[dict]) -> None:
     """
     GIVEN a registered user
     WHEN attempts to find out which user they are
     THEN check that the backend responds with a 200 response code
 
-    :param client: The flask app
-    :type client: flask.testing.FlaskClient
-    :param client_accounts: The dictionary of users
-    :type client_accounts: dict
+    Args:
+        client: The flask app
+        client_accounts: List of dict
     """
+
     for i in range(NUMBER_OF_USERS):
-        response: flask.Response = client.post("/auth/login", json=client_accounts[i])
+        response = client.post("/auth/login", json=client_accounts[i])
         assert response.status_code == HTTP_SUCCESS_CODE
 
-        response: flask.Response = client.get("/auth/which_user")
+        response = client.get("/auth/which_user")
         assert response.status_code == HTTP_SUCCESS_CODE
         assert response.get_json()["user_id"] == i + 1
 
 
 @pytest.mark.repeat(REPEAT_TESTS)
-def test_which_user_invalid(client, client_accounts) -> None:
+def test_which_user_invalid(client: FlaskClient, client_accounts: list[dict]) -> None:
     """
-    GIVEN a registered user
+    GIVEN a non-registered user who has cleared their cookies
     WHEN attempts to find out which user they are
-    THEN check that the backend does not responds with a 200 response code
+    THEN check that the backend does not respond with a 200 response code
 
-    :param client: The flask app
-    :type client: flask.testing.FlaskClient
-    :param client_accounts: The dictionary of users
-    :type client_accounts: dict
+    Args:
+        client: The flask app
+        client_accounts: List of dict
+
     """
     client.cookie_jar.clear()
-    response: flask.Response = client.get("/auth/which_user")
+    response = client.get("/auth/which_user")
     assert response.status_code != HTTP_SUCCESS_CODE
 
 
