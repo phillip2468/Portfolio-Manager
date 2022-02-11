00e161f1e6f0d9719d622e234bf7dbc0a2d6bdc9
Phillip Davis <phillipdavis2468@gmail.com>
2022-02-11 21:54:29 +1100

    docstrings to get portfolio details

--- backend/money_maker/portfolio/test_routes/test_get_portfolio_details.py
+++ backend/money_maker/portfolio/test_routes/test_get_portfolio_details.py
@@ -1,24 +1,23 @@
+from flask.testing import FlaskClient
+
 from conftest import HTTP_SUCCESS_CODE
 from money_maker.extensions import db
 from money_maker.models.portfolio import Portfolio
 
 
-def test_get_all_portfolios(client, client_accounts, symbols, stock_prices, logged_in_user_id):
+def test_get_all_portfolios(client: FlaskClient, client_accounts: list[dict], symbols: None,
+                            stock_prices: None, logged_in_user_id: int) -> None:
     """
     GIVEN a user id
     WHEN a user is logged in
     THEN check that all portfolios are returned for the user
 
-    :param client: The flask testing client
-    :type client: FlaskClient
-    :param client_accounts: The users in the database
-    :type client_accounts: dict
-    :param symbols: The stock ticker symbols
-    :type symbols: Any
-    :param stock_prices: A fixture which updates all the stock prices from the symbol's fixture
-    :type stock_prices: Any
-    :param logged_in_user_id: The id of the user
-    :type logged_in_user_id: int
+    Args:
+        client: The flask application
+        client_accounts: The list of dictionaries of clients
+        symbols: The query to create stock symbol rows
+        stock_prices: The query to update up to 100 stocks of alphabetically
+        logged_in_user_id: The logged-in user_id
     """
 
     for i in range(1, 5):
@@ -33,22 +32,21 @@ def test_get_all_portfolios(client, client_accounts, symbols, stock_prices, logg
     assert len(response.get_json()) > 1
 
 
-def test_get_all_stocks_from_portfolio(client, client_accounts, symbols, stock_prices, logged_in_user_id, sample_portfolio):
+def test_get_all_stocks_from_portfolio(client: FlaskClient, client_accounts: list[dict], symbols: None,
+                                       stock_prices: None, logged_in_user_id: int, sample_portfolio: str) -> None:
     """
     GIVEN a user id
     WHEN a user is logged in
     THEN check that all stocks from a portfolios are returned for the user
 
-    :param client: The flask testing client
-    :type client: FlaskClient
-    :param client_accounts: The users in the database
-    :type client_accounts: dict
-    :param symbols: The stock ticker symbols
-    :type symbols: Any
-    :param stock_prices: A fixture which updates all the stock prices from the symbol's fixture
-    :type stock_prices: Any
-    :param logged_in_user_id: The id of the user
-    :type logged_in_user_id: int
+    Args:
+        client: The flask application
+        client_accounts: The list of dictionaries of clients
+        symbols: The query to create stock symbol rows
+        stock_prices: The query to update up to 100 stocks of alphabetically
+        logged_in_user_id: The logged-in user_id
+        sample_portfolio: Places sample portfolio into database portfolio
+
     """
 
     assert len(db.session.query(Portfolio)
