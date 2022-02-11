e1c7a27ee36389e24041e62c60097eb805f849b3
Phillip Davis <phillipdavis2468@gmail.com>
2022-02-11 22:01:17 +1100

    added type hints to logout and register testing testing_routes

--- backend/money_maker/auth/test_routes/test_logout.py
+++ backend/money_maker/auth/test_routes/test_logout.py
@@ -1,16 +1,20 @@
 import pytest
+from flask.testing import FlaskClient
+
 from conftest import HTTP_SUCCESS_CODE, NUMBER_OF_USERS, REPEAT_TESTS
 
 
 @pytest.mark.repeat(REPEAT_TESTS)
-def test_valid_logout(client, client_accounts) -> None:
+def test_valid_logout(client: FlaskClient, client_accounts: list[dict]) -> None:
     """
     GIVEN a user
     WHEN a User logout
     THEN check that the backend provides a 200 response
 
-    :param client: the flask app
-    :param client_accounts: a dictionary containing the user details
+    Args:
+        client: The flask application
+        client_accounts: The list of dictionaries of clients
+
     """
     for i in range(NUMBER_OF_USERS):
         response = client.post("/auth/login", json=client_accounts[i])
--- backend/money_maker/auth/test_routes/test_register.py
+++ backend/money_maker/auth/test_routes/test_register.py
@@ -1,6 +1,8 @@
 import random
 
 import pytest
+from flask.testing import FlaskClient
+
 from conftest import (HTTP_SUCCESS_CODE, LETTER_CASINGS, MAX_LENGTH_EMAIL,
                       MIN_LENGTH_EMAIL, REPEAT_TESTS)
 from money_maker.extensions import db, faker_data
@@ -8,15 +10,15 @@ from money_maker.models.user import User
 
 
 @pytest.mark.repeat(REPEAT_TESTS)
-def test_valid_register(client):
+def test_valid_register(client: FlaskClient) -> None:
     """
     GIVEN valid user_details
     WHEN a new user registers
     THEN check that the user exists in the database by checking
     the length of rows.
 
-    :param client: The flask app fixture
-    :type client: Any
+    Args:
+        client: The flask application
     """
     body = {
         "email": faker_data.ascii_email(),
@@ -28,15 +30,16 @@ def test_valid_register(client):
 
 
 @pytest.mark.repeat(REPEAT_TESTS)
-def test_valid_multiple_register(client):
+def test_valid_multiple_register(client: FlaskClient) -> None:
     """
     GIVEN multiple Users
     WHEN each User registers
     THEN check that each user in the database exists by checking
     the length of rows in the database.
 
-    :param client: The flask app fixture
-    :type client: Any
+    Args:
+        client: The flask application
+
     """
     random_iterations = random.randint(2, 10)
     for i in range(random_iterations):
@@ -53,14 +56,15 @@ def test_valid_multiple_register(client):
 
 
 @pytest.mark.repeat(REPEAT_TESTS)
-def test_invalid_register_email(client):
+def test_invalid_register_email(client: FlaskClient) -> None:
     """
     GIVEN a user with a first name as their email address
     WHEN a User registers
     THEN check that the model raises a valuerror and does not insert the user
 
-    :param client: The flask app fixture
-    :type client: Any
+    Args:
+        client: The flask application
+
     """
 
     with pytest.raises(ValueError):
@@ -78,14 +82,15 @@ def test_invalid_register_email(client):
 
 
 @pytest.mark.repeat(REPEAT_TESTS)
-def test_invalid_register_short_pw(client):
+def test_invalid_register_short_pw(client: FlaskClient) -> None:
     """
-    GIVEN a user with a password between 4 to 7 characters
+    GIVEN a user with a password between 4 and 7 characters
     WHEN a User registers
     THEN check that the model raises a valuerror and does not insert the user
 
-    :param client: The flask app fixture
-    :type client: flask.app.Flask
+    Args:
+        client: The flask application
+
     """
     with pytest.raises(ValueError):
         casing = random.choice(LETTER_CASINGS)
@@ -102,14 +107,14 @@ def test_invalid_register_short_pw(client):
 
 
 @pytest.mark.repeat(REPEAT_TESTS)
-def test_invalid_register_passwords(client):
+def test_invalid_register_passwords(client: FlaskClient) -> None:
     """
     GIVEN a user with a password that contains special characters
     WHEN a User registers
     THEN check that the model raises a valuerror and does not insert the user
 
-    :param client: The flask app fixture
-    :type client: flask.app.Flask
+    Args:
+        client: The flask application
     """
 
     with pytest.raises(ValueError):
