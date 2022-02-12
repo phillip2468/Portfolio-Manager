7e147b641c73e73c6cb5bd14ddc6fc81d21104b7
Phillip Davis <phillipdavis2468@gmail.com>
2022-02-12 21:48:08 +1100

    test which user working

--- backend/money_maker/auth/test_routes/test_register.py
+++ backend/money_maker/auth/test_routes/test_register.py
@@ -1,11 +1,12 @@
 import random
 
 import pytest
-from conftest import (HTTP_SUCCESS_CODE, LETTER_CASINGS, MAX_LENGTH_EMAIL,
-                      MIN_LENGTH_EMAIL, NUMBER_OF_USERS, REPEAT_TESTS)
 from flask.testing import FlaskClient
 from hypothesis import assume, given
 from hypothesis import strategies as st
+
+from conftest import (HTTP_SUCCESS_CODE, MAX_LENGTH_EMAIL,
+                      MIN_LENGTH_EMAIL, NUMBER_OF_USERS, REPEAT_TESTS)
 from money_maker.extensions import db, faker_data
 from money_maker.models.user import User
 
@@ -66,7 +67,7 @@ def test_valid__mutliple_registers(flask_application: FlaskClient) -> None:
     assert len(db.session.query(User).all()) == 0
 
 
-@given(invalid_email=st.from_regex(r'^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$'))
+@given(invalid_email=st.emails())
 def test_invalid_register_email(flask_application: FlaskClient, invalid_email: str) -> None:
     assume(len(invalid_email) > 0)
     print(invalid_email)
@@ -82,52 +83,3 @@ def test_invalid_register_email(flask_application: FlaskClient, invalid_email: s
                                             )
         }
         flask_application.post("/auth/register", json=user)
-
-
-@pytest.mark.repeat(REPEAT_TESTS)
-def test_invalid_register_short_pw(client: FlaskClient) -> None:
-    """
-    GIVEN a user with a password between 4 and 7 characters
-    WHEN a User registers
-    THEN check that the model raises a valuerror and does not insert the user
-
-    Args:
-        client: The flask application
-
-    """
-    with pytest.raises(ValueError):
-        casing = random.choice(LETTER_CASINGS)
-        body = {
-            "email": faker_data.ascii_email(),
-            "password": faker_data.password(length=random.randint(4, 7),
-                                            special_chars=random.choice([True, False]),
-                                            digits=random.choice([True, False]),
-                                            upper_case=casing[0],
-                                            lower_case=casing[1]
-                                            )
-        }
-        client.post("/auth/register", json=body)
-
-
-@pytest.mark.repeat(REPEAT_TESTS)
-def test_invalid_register_passwords(client: FlaskClient) -> None:
-    """
-    GIVEN a user with a password that contains special characters
-    WHEN a User registers
-    THEN check that the model raises a valuerror and does not insert the user
-
-    Args:
-        client: The flask application
-    """
-
-    with pytest.raises(ValueError):
-        casing = random.choice(LETTER_CASINGS)
-        body = {
-            "email": faker_data.ascii_email(),
-            "password": faker_data.password(length=random.randint(MIN_LENGTH_EMAIL, MAX_LENGTH_EMAIL),
-                                            special_chars=True,
-                                            digits=random.choice([True, False]),
-                                            upper_case=casing[0],
-                                            lower_case=casing[1])
-        }
-        client.post("/auth/register", json=body)
--- backend/money_maker/auth/test_routes/test_which_user.py
+++ backend/money_maker/auth/test_routes/test_which_user.py
@@ -1,44 +1,37 @@
 import pytest
 from flask.testing import FlaskClient
 
-from conftest import HTTP_SUCCESS_CODE, NUMBER_OF_USERS, REPEAT_TESTS
+from conftest import HTTP_SUCCESS_CODE, REPEAT_TESTS
 
 
 @pytest.mark.repeat(REPEAT_TESTS)
-def test_which_user(client: FlaskClient, client_accounts: list[dict]) -> None:
+def test_which_user(flask_application: FlaskClient, user_account_logged_in: dict) -> None:
     """
     GIVEN a registered user
     WHEN attempts to find out which user they are
     THEN check that the backend responds with a 200 response code
 
     Args:
-        client: The flask app
-        client_accounts: List of dict
+        flask_application: The flask application
+        user_account_logged_in: The single registered user logged into the flask application.
     """
 
-    for i in range(NUMBER_OF_USERS):
-        response = client.post("/auth/login", json=client_accounts[i])
-        assert response.status_code == HTTP_SUCCESS_CODE
-
-        response = client.get("/auth/which_user")
-        assert response.status_code == HTTP_SUCCESS_CODE
-        assert response.get_json()["user_id"] == i + 1
+    response = flask_application.get("/auth/which_user")
+    assert response.status_code == HTTP_SUCCESS_CODE
+    assert response.get_json()["user_id"] == 1
 
 
 @pytest.mark.repeat(REPEAT_TESTS)
-def test_which_user_invalid(client: FlaskClient, client_accounts: list[dict]) -> None:
+def test_invalid_which_user_no_cookies(flask_application: FlaskClient, user_account_logged_in: dict) -> None:
     """
-    GIVEN a non-registered user who has cleared their cookies
+    GIVEN a registered user who has cleared their cookies
     WHEN attempts to find out which user they are
     THEN check that the backend does not respond with a 200 response code
 
     Args:
-        client: The flask app
-        client_accounts: List of dict
-
+        flask_application: The flask application
+        user_account_logged_in: The single registered user logged into the flask application.
     """
-    client.cookie_jar.clear()
-    response = client.get("/auth/which_user")
+    flask_application.cookie_jar.clear()
+    response = flask_application.get("/auth/which_user")
     assert response.status_code != HTTP_SUCCESS_CODE
-
-
