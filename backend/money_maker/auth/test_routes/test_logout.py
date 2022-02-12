92514881269a6e831cabe6e30d27f29a2cd654df
phillip2468 <54766922+phillip2468@users.noreply.github.com>
2022-02-12 20:58:13 +1100

    multiple users logging out successful

--- backend/money_maker/auth/test_routes/test_logout.py
+++ backend/money_maker/auth/test_routes/test_logout.py
@@ -18,3 +18,23 @@ def test_valid_logout(flask_application: FlaskClient, user_account_logged_in: di
     response = flask_application.post("/auth/logout")
     assert response.status_code == HTTP_SUCCESS_CODE
     assert response.get_json()["msg"] == "logout successful"
+
+
+@pytest.mark.repeat(REPEAT_TESTS)
+def test_multiple_valid_logouts(flask_application: FlaskClient, user_accounts: dict) -> None:
+    """
+    GIVEN a list of registered users
+    WHEN this user attempts to log out
+    THEN check that the backend provides a 200 response
+    Args:
+        flask_application (FlaskClient):  The flask application
+        user_accounts (list[dict]): A list of registered users
+    """
+
+    for user in user_accounts:
+        response = flask_application.post("/auth/login", json=user)
+        assert response.status_code == 200
+
+        response = flask_application.post("/auth/logout")
+        assert response.status_code == HTTP_SUCCESS_CODE
+        assert response.get_json()["msg"] == "logout successful"
