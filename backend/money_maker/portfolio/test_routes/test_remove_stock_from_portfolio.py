260dfe42504600fb498500681f1fe4e53360c6f7
Phillip Davis <phillipdavis2468@gmail.com>
2022-02-12 23:01:44 +1100

    need to add more validators to the portfolio's routes

--- backend/money_maker/portfolio/test_routes/test_remove_stock_from_portfolio.py
+++ backend/money_maker/portfolio/test_routes/test_remove_stock_from_portfolio.py
@@ -1,35 +1,36 @@
+import pytest
 from flask.testing import FlaskClient
 
-from conftest import HTTP_SUCCESS_CODE
+from conftest import HTTP_SUCCESS_CODE, REPEAT_TESTS
 from money_maker.extensions import db
 from money_maker.models.portfolio import Portfolio
 
+NUMBER_OF_STOCKS = 10
 
-def test_remove_stock_from_portfolio(client: FlaskClient, client_accounts: list[dict], symbols: None,
-                                     stock_prices: None, logged_in_user_id: int) -> None:
+
+@pytest.mark.repeat(REPEAT_TESTS)
+def test_remove_stock_from_portfolio(flask_application: FlaskClient, user_account_logged_in: dict,
+                                     user_id: int, sample_portfolio: str) -> None:
     """
     GIVEN a stock id
     WHEN a user is logged in and wants to remove a stock from a portfolio
     THEN check that the new stock has been removed
 
     Args:
-        client: The flask application
-        client_accounts: The list of dictionaries of clients
-        symbols: The query to create stock symbol rows
-        stock_prices: The query to update up to 100 stocks of alphabetically
-        logged_in_user_id: The logged-in user_id
-
+        flask_application: The flask application
+        user_account_logged_in: The single registered user logged in.
+        user_id: The id of the user
+        sample_portfolio: The name of the sample portfolio
     """
-
-    response = client.post(f"""/portfolio/{logged_in_user_id}/sample_portfolio""")
+    response = flask_application.post(f"""/portfolio/{user_id}/{sample_portfolio}/1""")
     assert response.status_code == HTTP_SUCCESS_CODE
+    assert response.get_json()["msg"] == "Successfully added stock"
 
-    response = client.post(f"""/portfolio/{logged_in_user_id}/sample_portfolio/1""")
+    response = flask_application.delete(f"""/portfolio/{user_id}/{sample_portfolio}/1""")
     assert response.status_code == HTTP_SUCCESS_CODE
+    assert response.get_json()["msg"] == "Successfully deleted the stock"
 
-    assert len(db.session.query(Portfolio)
-               .filter(Portfolio.user_id == logged_in_user_id, Portfolio.stock_id == 1).all()) == 1
+    # Remember that the empty portfolio still exists in the database
+    assert len(db.session.query(Portfolio.user_id == user_id).all()) == 1
 
-    response = client.delete(f"""/portfolio/{logged_in_user_id}/sample_portfolio/1""")
-    assert response.status_code == HTTP_SUCCESS_CODE
-    assert len(db.session.query(Portfolio).filter(Portfolio.stock_id == 1).all()) == 0
+
