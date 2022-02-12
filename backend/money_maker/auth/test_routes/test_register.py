04e03476d53ef12735bdccc5dcc9797b48cf5dee
phillip2468 <54766922+phillip2468@users.noreply.github.com>
2022-02-12 21:08:44 +1100

    registering users now works

--- backend/money_maker/auth/test_routes/test_register.py
+++ backend/money_maker/auth/test_routes/test_register.py
@@ -1,16 +1,15 @@
 import random
 
 import pytest
-from flask.testing import FlaskClient
-
 from conftest import (HTTP_SUCCESS_CODE, LETTER_CASINGS, MAX_LENGTH_EMAIL,
-                      MIN_LENGTH_EMAIL, REPEAT_TESTS)
+                      MIN_LENGTH_EMAIL, NUMBER_OF_USERS, REPEAT_TESTS)
+from flask.testing import FlaskClient
 from money_maker.extensions import db, faker_data
 from money_maker.models.user import User
 
 
 @pytest.mark.repeat(REPEAT_TESTS)
-def test_valid_register(client: FlaskClient) -> None:
+def test_valid_register(flask_application: FlaskClient) -> None:
     """
     GIVEN valid user_details
     WHEN a new user registers
@@ -18,15 +17,51 @@ def test_valid_register(client: FlaskClient) -> None:
     the length of rows.
 
     Args:
-        client: The flask application
+        flask_application (FlaskClient): The flask application
     """
     body = {
         "email": faker_data.ascii_email(),
         "password": faker_data.password(length=10, special_chars=False)
     }
-    response = client.post("/auth/register", json=body)
+    response = flask_application.post("/auth/register", json=body)
     assert response.status_code == HTTP_SUCCESS_CODE
+    assert response.get_json()["msg"] == "register successful"
     assert len(db.session.query(User).all()) == 1
+
+    db.session.query(User).filter(User.email == body["email"]).delete(synchronize_session="fetch")
+    db.session.commit()
+
+    assert len(db.session.query(User).all()) == 0
+
+
+@pytest.mark.repeat(REPEAT_TESTS)
+def test_valid__mutliple_registers(flask_application: FlaskClient) -> None:
+    """
+    GIVEN multiple valid user details
+    WHEN each user attempts to log in
+    THEN check that the user exists in the database by checking
+    the length of rows.
+
+    Args:
+        flask_application (FlaskClient): The flask application
+    """
+    list_of_users = [{
+        "email": faker_data.ascii_email(),
+        "password": faker_data.password(length=10, special_chars=False)
+    } for _ in range(NUMBER_OF_USERS)]
+
+    for index, user in enumerate(list_of_users):
+        response = flask_application.post("/auth/register", json=user)
+        assert response.status_code == HTTP_SUCCESS_CODE
+        assert response.get_json()["msg"] == "register successful"
+        # Remember that ranges have a zero based index!
+        assert len(db.session.query(User).all()) == index + 1
+
+    for user in list_of_users:
+        db.session.query(User).filter(User.email == user["email"]).delete(synchronize_session="fetch")
+        db.session.commit()
+
+    assert len(db.session.query(User).all()) == 0
 
 
 @pytest.mark.repeat(REPEAT_TESTS)
