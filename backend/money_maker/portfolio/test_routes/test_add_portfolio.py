7b1a7908deb0d115155a37c0098eccae25841802
Phillip Davis <phillipdavis2468@gmail.com>
2022-02-12 22:46:02 +1100

    pytests for adding stocks to portfolio completed

--- backend/money_maker/portfolio/test_routes/test_add_portfolio.py
+++ backend/money_maker/portfolio/test_routes/test_add_portfolio.py
@@ -21,6 +21,7 @@ def test_add_portfolio(flask_application: FlaskClient, user_account_logged_in: d
 
     response = flask_application.post(f"""/portfolio/{user_id}/example""")
     assert response.status_code == HTTP_SUCCESS_CODE
+    assert response.get_json()["msg"] == "Successfully created a new portfolio"
     assert len(db.session.query(Portfolio).all()) == 1
 
     db.session.query(Portfolio).filter(Portfolio.user_id == user_id).delete(synchronize_session="fetch")
@@ -28,20 +29,3 @@ def test_add_portfolio(flask_application: FlaskClient, user_account_logged_in: d
 
     assert len(db.session.query(Portfolio).all()) == 0
 
-
-@pytest.mark.repeat(REPEAT_TESTS)
-def test_invalid_add_empty_portfolio_name(flask_application: FlaskClient, user_account_logged_in: dict, user_id: int) \
-        -> None:
-    """
-    GIVEN a portfolio name that is empty
-    WHEN a user is logged in and wants to create a portfolio
-    THEN check that the new portfolio is not created.
-
-    Args:
-        flask_application: The flask application
-        user_account_logged_in: The single registered user logged in.
-        user_id: The id of the user
-    """
-
-    response = flask_application.post(f"""/portfolio/{user_id}/""")
-    assert response.status_code != HTTP_SUCCESS_CODE
--- backend/money_maker/portfolio/test_routes/test_add_stock_to_portfolio.py
+++ backend/money_maker/portfolio/test_routes/test_add_stock_to_portfolio.py
@@ -1,103 +1,111 @@
+import pytest
 from flask.testing import FlaskClient
 
-from conftest import HTTP_SUCCESS_CODE
-from money_maker.extensions import db, faker_data
+from conftest import HTTP_SUCCESS_CODE, REPEAT_TESTS
+from money_maker.extensions import db
 from money_maker.models.portfolio import Portfolio
 
+NUMBER_OF_PFS = 5
+NUMBER_OF_STOCKS = 10
 
-def test_add_stock_to_portfolio(client: FlaskClient, client_accounts: list[dict], symbols: None,
-                                stock_prices: None, logged_in_user_id: int, sample_portfolio: str) -> None:
+
+@pytest.mark.repeat(REPEAT_TESTS)
+def test_add_stock_to_portfolio(flask_application: FlaskClient, user_account_logged_in: dict,
+                                user_id: int, sample_portfolio: str) -> None:
     """
     GIVEN a stock id
     WHEN a user is logged in and wants to add a stock to portfolio
     THEN check that the new stock has been added
+
     Args:
-        client: The flask application
-        client_accounts: The list of dictionaries of clients
-        symbols: The query to create stock symbol rows
-        stock_prices: The query to update up to 100 stocks of alphabetically
-        logged_in_user_id: The logged-in user_id
-        sample_portfolio: Places sample portfolio into database portfolio
+        flask_application: The flask application
+        user_account_logged_in: The single registered user logged in.
+        user_id: The id of the user
+        sample_portfolio: The name of the sample portfolio
     """
 
-    response = client.post(f"""/portfolio/{logged_in_user_id}/{sample_portfolio}/1""")
+    response = flask_application.post(f"""/portfolio/{user_id}/{sample_portfolio}/1""")
     assert response.status_code == HTTP_SUCCESS_CODE
 
-    assert len(db.session.query(Portfolio)
-               .filter(Portfolio.user_id == logged_in_user_id, Portfolio.stock_id == 1).all()) == 1
+    db.session.query(Portfolio).filter(Portfolio.user_id == user_id).delete(synchronize_session="fetch")
+    db.session.commit()
+
+    assert len(db.session.query(Portfolio).all()) == 0
 
 
-def test_add_multiple_stocks_to_portfolio(client: FlaskClient, client_accounts: list[dict], symbols: None,
-                                          stock_prices: None, logged_in_user_id: int, sample_portfolio: str) -> None:
+def test_add_multiple_stocks_to_portfolio(flask_application: FlaskClient, user_account_logged_in: dict,
+                                          user_id: int, sample_portfolio: str) -> None:
     """
     GIVEN multiple stock_ids
     WHEN a user is logged in and wants to add a stock to portfolio
     THEN check that the new stock has been added
     Args:
-        client: The flask application
-        client_accounts: The list of dictionaries of clients
-        symbols: The query to create stock symbol rows
-        stock_prices: The query to update up to 100 stocks of alphabetically
-        logged_in_user_id: The logged-in user_id
-        sample_portfolio: Places sample portfolio into database portfolio
+        flask_application: The flask application
+        user_account_logged_in: The single registered user logged in.
+        user_id: The id of the user
+        sample_portfolio: The name of the sample portfolio
     """
-    response = client.post(f"""/portfolio/{logged_in_user_id}/{sample_portfolio}""")
-    assert response.status_code == HTTP_SUCCESS_CODE
 
-    for i in range(1, 9):
-        response = client.post(f"""/portfolio/{logged_in_user_id}/{sample_portfolio}/{i}""")
+    for i in range(1, 10):
+        response = flask_application.post(f"""/portfolio/{user_id}/{sample_portfolio}/{i}""")
         assert response.status_code == HTTP_SUCCESS_CODE
 
-    assert len(db.session.query(Portfolio).filter(Portfolio.user_id == logged_in_user_id).all()) == 10
+    assert len(db.session.query(Portfolio).filter(Portfolio.user_id == user_id).all()) == 10
+    db.session.query(Portfolio).filter(Portfolio.user_id == user_id).delete(synchronize_session="fetch")
+    db.session.commit()
+    assert len(db.session.query(Portfolio).all()) == 0
 
 
-def test_add_multiple_stocks_to_different_portfolios(client: FlaskClient, client_accounts: list[dict], symbols: None,
-                                                     stock_prices: None, logged_in_user_id: int, sample_portfolio: str) -> None:
+def test_add_multiple_stocks_to_different_portfolios(flask_application: FlaskClient, user_account_logged_in: dict,
+                                                     user_id: int) -> None:
     """
     GIVEN multiple stock_ids
     WHEN a user is logged in and wants to add a stock to a particular portfolio (and already has multiple portfolios)
     THEN check that the new stocks corresponds to the sample portfolio
     Args:
-        client: The flask application
-        client_accounts: The list of dictionaries of clients
-        symbols: The query to create stock symbol rows
-        stock_prices: The query to update up to 100 stocks of alphabetically
-        logged_in_user_id: The logged-in user_id
-        sample_portfolio: Places sample portfolio into database portfolio
+        flask_application: The flask application
+        user_account_logged_in: The single registered user logged in.
+        user_id: The id of the user
     """
-    response = client.post(f"""/portfolio/{logged_in_user_id}/{sample_portfolio}""")
-    assert response.status_code == HTTP_SUCCESS_CODE
-
-    response = client.post(f"""/portfolio/{logged_in_user_id}/sample_portfolio_2""")
-    assert response.status_code == HTTP_SUCCESS_CODE
-
-    for i in range(1, 9):
-        response = client.post(f"""/portfolio/{logged_in_user_id}/{sample_portfolio}/{i}""")
+    for i in range(NUMBER_OF_PFS):
+        response = flask_application.post(f"""/portfolio/{user_id}/sample_portfolio_{i}""")
         assert response.status_code == HTTP_SUCCESS_CODE
+        assert response.get_json()["msg"] == "Successfully created a new portfolio"
 
-    assert len(db.session.query(Portfolio).filter(Portfolio.user_id == logged_in_user_id,
-                                                  Portfolio.portfolio_name == sample_portfolio).all()) == 10
+    for pf_index in range(NUMBER_OF_PFS):
+        pf_name = f"""sample_portfolio_{pf_index}"""
+        for stock_index in range(1, NUMBER_OF_STOCKS):
+            response = flask_application.post(f"""/portfolio/{user_id}/{pf_name}/{stock_index}""")
+            assert response.status_code == HTTP_SUCCESS_CODE
+            assert response.get_json()["msg"] == "Successfully added stock"
+        assert len(db.session.query(Portfolio).filter(Portfolio.user_id == user_id,
+                                                      Portfolio.portfolio_name == pf_name).all()) == NUMBER_OF_STOCKS
+
+    assert len(db.session.query(Portfolio).filter(Portfolio.user_id ==
+                                                  user_id).all()) == NUMBER_OF_STOCKS * NUMBER_OF_PFS
+    db.session.query(Portfolio).filter(Portfolio.user_id == user_id).delete(synchronize_session="fetch")
+    db.session.commit()
+    assert len(db.session.query(Portfolio).all()) == 0
 
 
-def test_add_invalid_stock_to_portfolio(client: FlaskClient, client_accounts: list[dict], symbols: None,
-                                        stock_prices: None, logged_in_user_id: int, sample_portfolio: str) -> None:
+@pytest.mark.repeat(REPEAT_TESTS)
+def test_invalid_add_stock_to_portfolio(flask_application: FlaskClient, user_account_logged_in: dict,
+                                        user_id: int, sample_portfolio: str) -> None:
     """
-    GIVEN an invalid stock_id
-    WHEN a user is logged in and wants to add a stock to a particular portfolio
-    THEN check that the new stock HAS NOT been added
+    GIVEN a stock id of 0
+    WHEN a user is logged in and wants to add a stock to portfolio
+    THEN check that the new stock has been not been added
 
     Args:
-        client: The flask application
-        client_accounts: The list of dictionaries of clients
-        symbols: The query to create stock symbol rows
-        stock_prices: The query to update up to 100 stocks of alphabetically
-        logged_in_user_id: The logged-in user_id
-        sample_portfolio: Places sample portfolio into database portfolio
+        flask_application: The flask application
+        user_account_logged_in: The single registered user logged in.
+        user_id: The id of the user
+        sample_portfolio: The name of the sample portfolio
     """
-    response = client.post(f"""/portfolio/{logged_in_user_id}/{sample_portfolio}/-{faker_data.random_number()}""")
+
+    response = flask_application.post(f"""/portfolio/{user_id}/{sample_portfolio}/0""")
     assert response.status_code != HTTP_SUCCESS_CODE
 
-    # Remember that when a portfolio is created, no stocks are added (which still means there's the original row)
-    # so we must count it
-    assert len(db.session.query(Portfolio.stock_id)
-               .filter(Portfolio.user_id == logged_in_user_id).all()) == 1
+    # Remember that the original portfolio still has a length of 1!
+    assert len(db.session.query(Portfolio).all()) == 1
+
