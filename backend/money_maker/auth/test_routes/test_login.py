cea425a1a7c2fa8dc48fdf67b335a48e32b3f340
phillip2468 <54766922+phillip2468@users.noreply.github.com>
2022-02-12 19:55:12 +1100

    changed the way user_accounts are handled

--- backend/money_maker/auth/test_routes/test_login.py
+++ backend/money_maker/auth/test_routes/test_login.py
@@ -1,23 +1,20 @@
 import pytest
-from flask.testing import FlaskClient
-
 from conftest import HTTP_SUCCESS_CODE, NUMBER_OF_USERS, REPEAT_TESTS
+from flask.testing import FlaskClient
 
 
 @pytest.mark.repeat(REPEAT_TESTS)
-def test_valid_login(client: FlaskClient, client_accounts: list[dict]) -> None:
+def test_valid_login(flask_application: FlaskClient, user_account: dict) -> None:
     """
     GIVEN a registered user
     WHEN a User logins
     THEN check that the backend responds with a 200 response code
     Args:
-        client: The flask application
-        client_accounts: The list of dictionaries of clients
+        flask_application: The flask application
+        user_account: The list of dictionaries of clients
     """
-
-    for i in range(NUMBER_OF_USERS):
-        response = client.post("/auth/login", json=client_accounts[i])
-        assert response.status_code == HTTP_SUCCESS_CODE
+    response = flask_application.post("/auth/login", json=user_account)
+    assert response.status_code == HTTP_SUCCESS_CODE
 
 
 @pytest.mark.repeat(REPEAT_TESTS)
