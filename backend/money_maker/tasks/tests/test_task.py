0e962e3d84d7f269ca28615e0094e685569bb821
Phillip Davis <phillipdavis2468@gmail.com>
2022-02-13 15:32:04 +1100

    all tests now pass

--- backend/money_maker/tasks/tests/test_task.py
+++ backend/money_maker/tasks/tests/test_task.py
@@ -1,16 +1,18 @@
+from flask.testing import FlaskClient
+
 from conftest import HTTP_SUCCESS_CODE
 from money_maker.extensions import db
 from money_maker.models.ticker_prices import TickerPrice
 
 
-def test_get_stock_information(client, symbols):
+def test_get_stock_information(flask_application: FlaskClient) -> None:
     """
     GIVEN the flask client
     WHEN a request is submitted to check all stock prices
     THEN check that the stock was successfully inserted into the database (in this case since
     the first 100 stocks are updated AAPL is checked to see if inserted)
     """
-    response = client.get("/task")
+    response = flask_application.get("/task")
     assert response.status_code == HTTP_SUCCESS_CODE
     assert len(db.session.query(TickerPrice).all()) > 1
     assert len(db.session.query(TickerPrice).filter(TickerPrice.symbol == 'AAPL').all()) == 1
