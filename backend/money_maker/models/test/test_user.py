75342b1f6210b7484f87bca9345421f868470288
Phillip Davis <phillipdavis2468@gmail.com>
2022-02-13 17:19:53 +1100

    added some more tests to the models

--- backend/money_maker/models/test/test_user.py
+++ backend/money_maker/models/test/test_user.py
@@ -1,17 +1,41 @@
-from money_maker.extensions import bcrypt, faker_data
+import pytest
+
+from money_maker.extensions import faker_data
 from money_maker.models.user import User
 
 
-def test_text():
+@pytest.mark.repeat(100)
+def test_valid_register_user_details():
     """
     GIVEN valid user_details
     WHEN a new user is created
     THEN check that the email and password are created
-
     """
-    email = ""
+    email = faker_data.ascii_email()
     password = faker_data.password(length=10, special_chars=False)
-    new_user = User(email=email, hashed_password=password)
-    assert new_user.email == email
-    assert bcrypt.check_password_hash(new_user.hashed_password, password)
-    
+    assert User(email=email, hashed_password=password) is not None
+
+
+def test_invalid_email_register() -> None:
+    """
+    GIVEN an invalid email for user details
+    WHEN a new user is created
+    THEN check that the new account is NOT created.
+    """
+    email = faker_data.first_name()
+    password = faker_data.password(length=10, special_chars=False)
+    with pytest.raises(ValueError):
+        assert User(email=email, hashed_password=password) is None
+
+
+def test_invalid_password_register() -> None:
+    """
+    GIVEN an invalid password for user details
+    WHEN a new user is created
+    THEN check that the new account is NOT created.
+    """
+    email = faker_data.first_name()
+    password = faker_data.password(length=10, special_chars=True)
+    with pytest.raises(ValueError):
+        assert User(email=email, hashed_password=password) is None
+
