1f00b73aad6ad5ed6b967b32909dd89718447708
phillip2468 <54766922+phillip2468@users.noreply.github.com>
2022-02-13 19:41:37 +1100

    forgot to add alembic migrations

---
+++ backend/money_maker/models/test/test_portfolio.py
@@ -0,0 +1,48 @@
+import random
+
+import pytest
+from conftest import REPEAT_TESTS
+from marshmallow import ValidationError
+from money_maker.extensions import faker_data
+from money_maker.models.portfolio import Portfolio, portfolio_schema
+
+
+@pytest.mark.repeat(REPEAT_TESTS)
+def test_valid_portfolio_create(user_id: int) -> None:
+    """
+    GIVEN valid portfolio
+    WHEN portfolio needs to be created
+    THEN check the portflio can be successfully created
+    """
+    random_length = random.randint(1, 16)
+    print(user_id)
+    pf_data = {
+        "portfolio_name": faker_data.lexify(text=random_length*"?"),
+        "user_id": f"""{user_id}"""
+    }
+    result = portfolio_schema.load(data=pf_data, partial=True)
+    assert result is not None
+
+
+def test_invalid_portfolio_empty_name_create(user_id: int) -> None:
+    """
+    GIVEN an invalid portfolio name
+    WHEN portfolio needs to be created
+    THEN check the portflio CANNOT be successfully created
+    """
+    with pytest.raises(ValueError):
+        assert Portfolio(portfolio_name=faker_data.lexify(text=""), user_id=user_id) is None
+
+
+def test_invalid_portfolio_no_user_create() -> None:
+    """
+    GIVEN a portfolio
+    WHEN a portfolio is created without a user
+    THEN check the portfolio is not created
+    """
+    with pytest.raises(ValidationError):
+        random_length = random.randint(1, 16)
+        pf = Portfolio(portfolio_name=faker_data.lexify(text=random_length*"?"))
+        portfolio_schema.load(pf)
+        assert pf is None
+
