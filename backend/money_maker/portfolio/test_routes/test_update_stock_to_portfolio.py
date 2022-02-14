a482321a4d62650ee2105f49d37eac7e85f38941
Phillip Davis <phillipdavis2468@gmail.com>
2022-02-14 22:41:35 +1100

    added a pytest to make sure that even with multiple portfolios to a user, with just one stock update, that alone is updated

--- backend/money_maker/portfolio/test_routes/test_update_stock_to_portfolio.py
+++ backend/money_maker/portfolio/test_routes/test_update_stock_to_portfolio.py
@@ -1,6 +1,6 @@
 from flask.testing import FlaskClient
 
-from conftest import HTTP_SUCCESS_CODE, UPDATE_STOCK_TO_PORTFOLIO, ADD_STOCK_TO_PORTFOLIO
+from conftest import HTTP_SUCCESS_CODE, UPDATE_STOCK_TO_PORTFOLIO, ADD_STOCK_TO_PORTFOLIO, CREATE_PORTFOLIO_MSG
 from money_maker.extensions import db
 from money_maker.models.portfolio import Portfolio, portfolio_schema
 
@@ -81,3 +81,65 @@ def test_update_portfolio_one_stock_details(flask_application: FlaskClient, user
         pf_stock_object = portfolio_schema.dump(pf_stock)
         assert pf_stock_object["units_price"] == 0
         assert pf_stock_object["units_purchased"] == 0
+
+
+def test_one_update_portfolio_one_stock_details(flask_application: FlaskClient, user_account_logged_in: dict,
+                                                user_id: int) -> None:
+    """
+    GIVEN a stock id and a portfolio and the user has multiple portfolios
+    WHEN a user is logged in and wants to update the stock price and units of one stock
+    THEN check that the new details have been updated for that particular stock, and that
+    no other rows are affected.
+
+    Args:
+        flask_application: The flask application
+        user_account_logged_in: The single registered user logged in.
+        user_id: The id of the user
+
+    """
+    for pf in range(1, 5):
+        response = flask_application.post(f"""/portfolio/{user_id}/sample_pf_{pf}""")
+        assert response.status_code == HTTP_SUCCESS_CODE
+        assert response.get_json()["msg"] == CREATE_PORTFOLIO_MSG
+        for stock_id in range(1, 10):
+            response = flask_application.post(f"""/portfolio/{user_id}/sample_pf_{pf}/{stock_id}""")
+            assert response.status_code == HTTP_SUCCESS_CODE
+            assert response.get_json()["msg"] == ADD_STOCK_TO_PORTFOLIO
+
+    updated_stock_details = {
+        "units_price": 1000,
+        "units_purchased": 1000
+    }
+
+    response = flask_application.patch(f"""/portfolio/{user_id}/sample_pf_1/1""", json=updated_stock_details)
+    assert response.status_code == HTTP_SUCCESS_CODE
+    assert response.get_json()["msg"] == UPDATE_STOCK_TO_PORTFOLIO
+
+    changed_stock = db.session.query(Portfolio).filter(Portfolio.user_id == user_id,
+                                                       Portfolio.portfolio_name == "sample_pf_1",
+                                                       Portfolio.stock_id == 1).one_or_none()
+    as_object_stock = portfolio_schema.dump(changed_stock)
+    assert as_object_stock["units_price"] == 1000
+    assert as_object_stock["units_purchased"] == 1000
+
+    for stock_id in range(2, 10):
+        stock = db.session.query(Portfolio).filter(Portfolio.user_id == user_id,
+                                                   Portfolio.portfolio_name == "sample_pf_1",
+                                                   Portfolio.stock_id == stock_id).one_or_none()
+        assert stock is not None
+        pf_stock_object = portfolio_schema.dump(stock)
+        assert pf_stock_object["units_price"] == 0
+        assert pf_stock_object["units_purchased"] == 0
+
+    for pf in range(2, 5):
+        for stock_id in range(1, 10):
+            stock = db.session.query(Portfolio).filter(Portfolio.user_id == user_id,
+                                                       Portfolio.portfolio_name == f"""sample_pf_{pf}""",
+                                                       Portfolio.stock_id == stock_id).one_or_none()
+            assert stock is not None
+            pf_stock_object = portfolio_schema.dump(stock)
+            assert pf_stock_object["units_price"] == 0
+            assert pf_stock_object["units_purchased"] == 0
+
+    db.session.query(Portfolio).delete(synchronize_session="fetch")
+    db.session.commit()
