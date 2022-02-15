394f6ee2235362cba9ae4ac1ed3af58a6553e4a7
Phillip Davis <phillipdavis2468@gmail.com>
2022-02-15 15:25:48 +1100

    added a test to make sure other users can't remove others watchlist

--- backend/money_maker/watchlist/test_routes/test_remove_watchlist.py
+++ backend/money_maker/watchlist/test_routes/test_remove_watchlist.py
@@ -1,6 +1,7 @@
 from flask.testing import FlaskClient
 
-from conftest import HTTP_SUCCESS_CODE, CREATE_WATCHLIST_MSG, cleanup_wl_db, DELETE_WATCHLIST_MSG, NUMBER_OF_WATCHLISTS
+from conftest import HTTP_SUCCESS_CODE, CREATE_WATCHLIST_MSG, cleanup_wl_db, DELETE_WATCHLIST_MSG, NUMBER_OF_WATCHLISTS, \
+    LOGIN_SUCCESS_MSG
 from money_maker.extensions import db
 from money_maker.models.watchlist import Watchlist
 
@@ -53,3 +54,36 @@ def test_remove_a_watchlist(flask_application: FlaskClient, user_id: int) -> Non
 
     cleanup_wl_db(user_id)
 
+
+def test_invalid_remove_watchlist_to_other_user(flask_application: FlaskClient, user_accounts: list[dict]) -> None:
+    """
+    GIVEN another user from the registered users list
+    WHEN this user attempts to remove a watchlist for another user
+    THEN check that the server responds with an error and that the watchlist remains
+    Args:
+        flask_application: The flask application
+        user_accounts: The list of registered users
+
+    """
+    response = flask_application.post("/auth/login", json=user_accounts[0])
+    assert response.status_code == HTTP_SUCCESS_CODE
+    assert response.get_json()["msg"] == LOGIN_SUCCESS_MSG
+
+    response = flask_application.get("/auth/which_user")
+    assert response.status_code == HTTP_SUCCESS_CODE
+    user_id = response.get_json()["user_id"]
+
+    response = flask_application.post(f"""/watchlist/{user_id}/sample_watchlist""")
+    assert response.status_code == HTTP_SUCCESS_CODE
+    assert response.get_json()["msg"] == CREATE_WATCHLIST_MSG
+
+    response = flask_application.post("/auth/login", json=user_accounts[1])
+    assert response.status_code == HTTP_SUCCESS_CODE
+    assert response.get_json()["msg"] == LOGIN_SUCCESS_MSG
+
+    response = flask_application.delete(f"""/watchlist/{user_id}/sample_watchlist""")
+    assert response.status_code != HTTP_SUCCESS_CODE
+
+    assert len(db.session.query(Watchlist).filter(Watchlist.user_id == user_id).all()) == 1
+
+    cleanup_wl_db(user_id)
