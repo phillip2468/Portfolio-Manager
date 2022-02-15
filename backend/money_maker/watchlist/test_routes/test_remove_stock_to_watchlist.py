35c468394015accf45b89a9cd663364561333385
Phillip Davis <phillipdavis2468@gmail.com>
2022-02-15 16:34:45 +1100

    final route for watchlist auth

--- backend/money_maker/watchlist/test_routes/test_remove_stock_to_watchlist.py
+++ backend/money_maker/watchlist/test_routes/test_remove_stock_to_watchlist.py
@@ -1,7 +1,7 @@
 from flask.testing import FlaskClient
 
 from conftest import NUMBER_OF_STOCKS, ADD_STOCK_TO_WATCHLIST, HTTP_SUCCESS_CODE, cleanup_wl_db, \
-    REMOVE_STOCK_TO_WATCHLIST, NUMBER_OF_WATCHLISTS, CREATE_WATCHLIST_MSG
+    REMOVE_STOCK_TO_WATCHLIST, NUMBER_OF_WATCHLISTS, CREATE_WATCHLIST_MSG, LOGIN_SUCCESS_MSG
 from money_maker.extensions import db
 from money_maker.models.watchlist import Watchlist
 
@@ -73,5 +73,45 @@ def test_remove_stocks_to_a_watchlist(flask_application: FlaskClient, user_id: i
         assert len(db.session.query(Watchlist).filter(
             Watchlist.watchlist_name == f"""sample_watchlist_{i}""",
         ).all()) == NUMBER_OF_STOCKS + 1
+
+    cleanup_wl_db(user_id)
+
+
+def test_invalid_remove_stock_from_watchlist(flask_application: FlaskClient, user_accounts: list[dict]) -> None:
+    """
+    GIVEN a valid watchlist name
+    WHEN a registered user adds stocks to a watchlist and then removes a stocks from a particular watchlist
+    that isn't theirs
+    THEN check that the watchlist stocks are not removed
+
+    Args:
+        flask_application: The flask application
+        user_accounts: The list of registered users
+    """
+
+    response = flask_application.post("/auth/login", json=user_accounts[0])
+    assert response.status_code == HTTP_SUCCESS_CODE
+    assert response.get_json()["msg"] == LOGIN_SUCCESS_MSG
+
+    response = flask_application.get("/auth/which_user")
+    assert response.status_code == HTTP_SUCCESS_CODE
+    user_id = response.get_json()["user_id"]
+
+    response = flask_application.post(f"""/watchlist/{user_id}/sample_watchlist""")
+    assert response.status_code == HTTP_SUCCESS_CODE
+    assert response.get_json()["msg"] == CREATE_WATCHLIST_MSG
+
+    response = flask_application.post(f"""/watchlist/{user_id}/sample_watchlist/1""")
+    assert response.status_code == HTTP_SUCCESS_CODE
+    assert response.get_json()["msg"] == ADD_STOCK_TO_WATCHLIST
+
+    response = flask_application.post("/auth/login", json=user_accounts[1])
+    assert response.status_code == HTTP_SUCCESS_CODE
+    assert response.get_json()["msg"] == LOGIN_SUCCESS_MSG
+
+    response = flask_application.delete(f"""/watchlist/{user_id}/sample_watchlist/1""")
+    assert response.status_code != HTTP_SUCCESS_CODE
+
+    assert len(db.session.query(Watchlist).filter(Watchlist.user_id == user_id,).all()) == 2
 
     cleanup_wl_db(user_id)
