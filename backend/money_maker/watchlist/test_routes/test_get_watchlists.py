2041f750fcb234afc2da56a378f32f84708cd650
Phillip Davis <phillipdavis2468@gmail.com>
2022-02-15 15:03:12 +1100

    added a test for getting watchlists

--- backend/money_maker/watchlist/test_routes/test_get_watchlists.py
+++ backend/money_maker/watchlist/test_routes/test_get_watchlists.py
@@ -18,7 +18,7 @@ def test_get_all_watchlists(flask_application: FlaskClient, user_id: int) -> Non
         user_id: The id of the user
 
     """
-    for i in range(1, NUMBER_OF_WATCHLISTS):
+    for i in range(0, NUMBER_OF_WATCHLISTS):
         response = flask_application.post(f"""/watchlist/{user_id}/{wl_name}{i}""")
         assert response.status_code == HTTP_SUCCESS_CODE
         assert response.get_json()["msg"] == CREATE_WATCHLIST_MSG
@@ -27,8 +27,23 @@ def test_get_all_watchlists(flask_application: FlaskClient, user_id: int) -> Non
     assert response.status_code == HTTP_SUCCESS_CODE
     assert len(response.get_json()) == NUMBER_OF_WATCHLISTS
 
+    db.session.query(Watchlist).filter(Watchlist.user_id == user_id).delete(synchronize_session="fetch")
+    db.session.commit()
+
+    assert len(db.session.query(Watchlist.user_id == user_id).all()) == 0
+
 
 def test_invalid_user_get_all_watchlists(flask_application: FlaskClient, user_accounts: list[dict]) -> None:
+    """
+    GIVEN another user from the registered users list
+    WHEN this user attempts to get all watchlist from the other user
+    THEN check that the server responds with an error (since they do not have permission to view other users)
+
+    Args:
+        flask_application: The flask application
+        user_accounts: The list of registered accounts
+        
+    """
     response = flask_application.post("/auth/login", json=user_accounts[0])
     assert response.status_code == HTTP_SUCCESS_CODE
 
