20fa02cd46227ff02fa283eeb7ea50aff0ee8e0c
Phillip Davis <phillipdavis2468@gmail.com>
2022-02-15 16:28:46 +1100

    added a route to make sure that even with multiple watchlists, only that stock is removed

--- backend/money_maker/watchlist/test_routes/test_remove_stock_to_watchlist.py
+++ backend/money_maker/watchlist/test_routes/test_remove_stock_to_watchlist.py
@@ -1,7 +1,7 @@
 from flask.testing import FlaskClient
 
 from conftest import NUMBER_OF_STOCKS, ADD_STOCK_TO_WATCHLIST, HTTP_SUCCESS_CODE, cleanup_wl_db, \
-    REMOVE_STOCK_TO_WATCHLIST
+    REMOVE_STOCK_TO_WATCHLIST, NUMBER_OF_WATCHLISTS, CREATE_WATCHLIST_MSG
 from money_maker.extensions import db
 from money_maker.models.watchlist import Watchlist
 
@@ -38,5 +38,40 @@ def test_remove_stocks_to_watchlist(flask_application: FlaskClient, user_id: int
 
     assert len(db.session.query(Watchlist).filter(Watchlist.watchlist_name == sample_watchlist
                                                   ).all()) == 1
+
+    cleanup_wl_db(user_id)
+
+
+def test_remove_stocks_to_a_watchlist(flask_application: FlaskClient, user_id: int) -> None:
+    """
+    GIVEN a valid watchlist name
+    WHEN a registered user adds stocks to a watchlist and then removes all the stocks from a particular watchlist
+    THEN check that the watchlist stocks are in the watchlist
+
+    Args:
+        flask_application: The flask application
+        user_id: The id of the user
+
+    """
+    for wl in range(0, NUMBER_OF_WATCHLISTS):
+        response = flask_application.post(f"""/watchlist/{user_id}/sample_watchlist_{wl}""")
+        assert response.status_code == HTTP_SUCCESS_CODE
+        assert response.get_json()["msg"] == CREATE_WATCHLIST_MSG
+        for stock in range(1, NUMBER_OF_STOCKS + 1):
+            response = flask_application.post(f"""/watchlist/{user_id}/sample_watchlist_{wl}/{stock}""")
+            assert response.status_code == HTTP_SUCCESS_CODE
+            assert response.get_json()["msg"] == ADD_STOCK_TO_WATCHLIST
+
+    response = flask_application.delete(f"""/watchlist/{user_id}/sample_watchlist_0/1""")
+    assert response.status_code == HTTP_SUCCESS_CODE
+    assert response.get_json()["msg"] == REMOVE_STOCK_TO_WATCHLIST
+
+    assert len(db.session.query(Watchlist).filter(Watchlist.watchlist_name == "sample_watchlist_0"
+                                                  ).all()) == NUMBER_OF_STOCKS
+
+    for i in range(1, NUMBER_OF_WATCHLISTS):
+        assert len(db.session.query(Watchlist).filter(
+            Watchlist.watchlist_name == f"""sample_watchlist_{i}""",
+        ).all()) == NUMBER_OF_STOCKS + 1
 
     cleanup_wl_db(user_id)
